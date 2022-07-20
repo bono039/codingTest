dbdffcda5372fc74652564f7a9ef9272d8ea4ca9
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2022-07-20 23:56:40 +0900

    Create sort.java
    문자열 내 문자 정렬

---
+++ CodeTree/sort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        str = sc.next();
+        
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        
+        String sortedStr = new String(chars);
+        System.out.println(sortedStr);
+
+    }
+}
