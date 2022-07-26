02c6f502476fb2980ce86849a647706b204e5fe3
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2022-07-26 21:48:07 +0900

    별 찍기
    *****
    ** **
    * * *
    ** **
    *****

---
+++ CodeTree/draw.java
@@ -0,0 +1,26 @@
+package codeTree;
+
+import java.util.Scanner;
+
+public class draw {
+	
+	
+	public static void draw(int n) {
+	    for(int i=1 ; i<=n ; i++) {
+	        for(int j=1 ; j<=n ; j++) {
+	            if(i==1 || i==n || j==1 || j==n || j==i || (j ==(n - i + 1)))
+	                System.out.print("*");
+	            else
+	                System.out.print(" ");
+	        }
+	        System.out.println();
+	    }
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        draw(5);
+
+	}
+
+}
