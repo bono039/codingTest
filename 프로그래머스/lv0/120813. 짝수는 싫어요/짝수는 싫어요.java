f4fa7a05905e8fdd7b4357f53e7e5e98cada84a0
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-01-23 00:41:03 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        
+        for(int i=1 ; i<=n ; i+=2) {
+            answer[i/2] = i;
+        }
+        
+        return answer;
+    }
+}
