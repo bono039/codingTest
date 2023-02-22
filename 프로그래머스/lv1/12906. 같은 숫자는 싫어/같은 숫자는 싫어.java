bb27691e51eceb265db04efec8b1e8e78dd75894
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-02-22 20:45:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 35.74 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int val = -1;
+        
+        for(int i=0 ; i < arr.length ; i++) {
+            if(arr[i] != val) {
+                list.add(arr[i]);
+                val = arr[i];
+            }
+        }
+
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
