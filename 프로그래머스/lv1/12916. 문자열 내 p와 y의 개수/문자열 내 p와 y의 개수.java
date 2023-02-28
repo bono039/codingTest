4c1b88b694f650e10b287903c2934a9bf7c7c810
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-02-28 14:37:31 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -2,22 +2,16 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
 
-        int[] cnt = new int[2];
-        
-        s = s.toLowerCase();
+        int cntP = 0;
+        int cntY = 0;
         
         for(int i=0 ; i < s.length() ; i++) {
-            if(String.valueOf(s.charAt(i)).equals("p"))      cnt[0]++;
-            else if(String.valueOf(s.charAt(i)).equals("y")) cnt[1]++;
+            if(s.charAt(i) == 'P' || s.charAt(i) == 'p') cntP++;
+            else if(s.charAt(i) == 'Y' || s.charAt(i) == 'y') cntY++;
         }
-        
-        if(cnt[0] == cnt[1] && cnt[0]!= 0 && cnt[1] != 0) answer = true;
-        else if(cnt[0] != cnt[1])                         answer = false;
-        else if(cnt[0] == 0 && cnt[1] == 0)               answer = true;
-        
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
 
+        answer = (cntP == cntY) ? true : false;
+        
         return answer;
     }
 }
