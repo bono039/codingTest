132da3a20ef3f5c64a3b813adec501d6d26c4612
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-03-20 22:48:09 +0900

    [Silver III] Title: 통계학, Time: 1604 ms, Memory: 309588 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int n = sc.nextInt();
+	    int[] list = new int[n];   
+	    
+	    for(int i=0 ; i < n ; i++) {
+	        list[i] = sc.nextInt();
+	    }
+	    
+	    Arrays.sort(list); 
+	    
+	    // 산술평균
+	    double sum = 0;
+	    for(int i : list) {
+	        sum += i;
+	    }
+	    double result = Math.round(sum / n);
+	    System.out.println((int)Math.ceil(result));
+	    
+	    // 중앙값
+	    int mid = list[list.length/2];
+	    System.out.println(mid);
+	    
+	    // 최빈값 - 카운팅 정렬
+        int[] cnt = new int[8001];
+        int max = 0;
+        for(int i=0 ; i < list.length ; i++) {
+            cnt[list[i] + 4000]++;
+            max = Math.max(cnt[list[i] + 4000], max);
+        }
+        
+        ArrayList<Integer> max_idx = new ArrayList<>();
+        
+        for(int i=0 ; i <= 8000 ; i++) {
+            if(max == cnt[i])
+                max_idx.add(i-4000);
+        }
+        int how = (max_idx.size() > 1) ? max_idx.get(1) : max_idx.get(0);
+        System.out.println(how);
+        
+        // 범위
+        int range = list[n-1] - list[0];
+        System.out.println(range);
+	}
+}
