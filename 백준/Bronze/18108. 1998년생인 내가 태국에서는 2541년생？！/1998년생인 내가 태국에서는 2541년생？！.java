58b9d7c0f3fce01c0528e667cededaf31ec3f77f
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-03-22 12:05:41 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    var br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    int year = Integer.parseInt(br.readLine());
+        System.out.println(year - 543);
+	}
+}
