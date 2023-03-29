d6d3de866426683da7089c84c01ab9d881130dd2
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-03-30 00:27:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.60 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        
+        String[] str = s.split(" ");
+        int[] arr = new int[str.length];
+        
+        for(int i=0 ; i < str.length ; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        
+        Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[0]).append(" ").append(arr[arr.length -1]);
+        return sb.toString();
+    }
+}
