d22d9bef5422f2c40d4d1611f3c074a3a7ad2392
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-04-05 20:02:49 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,24 +1,23 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		var br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] arr = new int[26];
+		Arrays.fill(arr, -1);
+		String s = br.readLine();
 		
-		Scanner sc = new Scanner(System.in);
-		
-		int[] num = new int[26];
-		Arrays.fill(num, -1);
-		
-		String str = sc.nextLine();
-		
-		for(int i=0 ; i < str.length() ; i++) {
-		    char ch = str.charAt(i);
-		    
-		    if(num[ch - 'a'] == -1) {
-		        num[ch - 'a'] = i;
+		for(int i = 0 ; i < s.length() ; i++) {
+		    if(s.contains(String.valueOf(s.charAt(i)))) {
+		        if(arr[s.charAt(i) - 'a'] == -1) {
+		            arr[s.charAt(i) - 'a'] = i;
+		        }
 		    }
 		}
 		
-		for(int i : num) {
+		for(int i : arr) {
 		    System.out.print(i + " ");
 		}
 	}
