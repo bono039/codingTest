564213391b5cfb073505ec1605b6d1774b8e9718
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-04-06 17:34:49 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,23 +1,22 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
-        HashSet<String> set = new HashSet<String>();
+        int[] answer = new int[2];  // 사람, 차례
+        
+        Set<String> set = new HashSet<>();
         set.add(words[0]);
         
-        String prev = "";
-
-        for(int i = 1; i < words.length; i ++){
-            prev = words[i - 1];
-            
-            if(set.contains(words[i]) || prev.charAt(prev.length() - 1) != words[i].charAt(0)){
-                answer[0] = i % n + 1;	// 사람
-                answer[1] = i / n + 1;	// 차례
+        for(int i = 1 ; i < words.length ; i++) {
+            if(!set.contains(words[i]) && words[i-1].charAt(words[i-1].length() -1) == words[i].charAt(0)) {
+                set.add(words[i]);
+            } else {
+                answer[0] = i % n + 1;  // 사람
+                answer[1] = i / n +1;   // 차례
                 break;
             }
-            set.add(words[i]);
         }
+
         return answer;
     }
 }
