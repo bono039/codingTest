0f2e8e4706835613c4f1ad7b86d1baa109363d51
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-04-18 12:08:52 +0900

    [Silver IV] Title: 수 찾기, Time: 704 ms, Memory: 43516 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -3,24 +3,28 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-	    Scanner sc = new Scanner(System.in);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(br.readLine());
 		int[] A = new int[N];
 		
+		st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0 ; i < N ; i++) {
-		    A[i] = sc.nextInt();
+		    A[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		Arrays.sort(A); // 이진 탐색 위해 정렬
 		
 		// 존재하는지 비교할 숫자
-		int M = sc.nextInt();
+		int M = Integer.parseInt(br.readLine());
 		int[] B = new int[M];
 		
+		st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0 ; i < M ; i++) {
 		    boolean find = false;
-		    int target = sc.nextInt();
+		    int target = Integer.parseInt(st.nextToken());
 		    
 		    int start = 0;
 		    int end = A.length - 1;
@@ -41,8 +45,9 @@ public class Main {
 		        
 		    }
             
-            if(find) System.out.println(1);
-            else     System.out.println(0);
+            if(find) sb.append(1).append("\n");
+            else     sb.append(0).append("\n");
 		}
+		System.out.println(sb);
 	}
 }
