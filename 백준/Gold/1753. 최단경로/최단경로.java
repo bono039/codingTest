7e460807e72b304e084e1af439f11066050c3441
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-05-01 17:03:55 +0900

    [Gold IV] Title: 최단경로, Time: 980 ms, Memory: 110500 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -3,26 +3,26 @@ import java.io.*;
 
 public class Main {
     public static int V, E, K;
-    public static int[] D;         // 최단 거리 배열
-    public static boolean visited[]; // 방문 배열
+    public static int[] D;
+    public static boolean visited[];
     
-    public static ArrayList<ArrayList<Edge>> A; // 인접 리스트
+    public static ArrayList<ArrayList<Edge>> A;
     public static PriorityQueue<Edge> pq = new PriorityQueue<>();
     
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
-		V = Integer.parseInt(st.nextToken());   // 정점
-		E = Integer.parseInt(st.nextToken());   // 간선
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 		
-		K = Integer.parseInt(br.readLine());    // 시작점
+		K = Integer.parseInt(br.readLine());
 		
-		// 최단 거리 배열
+		visited = new boolean[V + 1];		
+		
 		D = new int[V + 1];
 		Arrays.fill(D, Integer.MAX_VALUE);
 		
-		// 인접 리스트
 		A = new ArrayList<>();
 		for(int i = 0 ; i <= V ; i++) {
 		    A.add(new ArrayList<>());
@@ -38,14 +38,15 @@ public class Main {
 		    A.get(u).add(new Edge(v, w));
 		}
 		
-		visited = new boolean[V + 1];
-		pq.add(new Edge(K, 0)); // K를 시작점으로 설정
+		
+		// 다익스트라 수행
+		pq.add(new Edge(K, 0));
 		D[K] = 0;
 		
 		while(!pq.isEmpty()) {
 		    Edge current = pq.poll();
 		    int c_ver = current.vertex;
-		    if(visited[c_ver]) continue;    // 중복 방지
+		    if(visited[c_ver]) continue;
 		    visited[c_ver] = true;
 		    
 		    for(int i = 0 ; i < A.get(c_ver).size() ; i++) {
@@ -54,7 +55,6 @@ public class Main {
 		        int next  = tmp.vertex;
 		        int value = tmp.value;
 		        
-		        // 최소 거리로 업데이트
 		        if(D[next] > D[c_ver] + value) {
 		            D[next] = D[c_ver] + value;
 		            pq.add(new Edge(next, D[next]));
@@ -80,7 +80,6 @@ class Edge implements Comparable<Edge> {
     }
     
     public int compareTo(Edge e) {
-        if(this.value > e.value) return 1;
-        else    return -1;
+        return Integer.compare(this.value, e.value);
     }
 }
