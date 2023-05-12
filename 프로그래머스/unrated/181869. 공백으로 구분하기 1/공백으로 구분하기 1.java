be6a5106e8ae6138806be801be09c76bc14de258
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-05-12 23:32:48 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.split(" ");
+        return answer;
+    }
+}
