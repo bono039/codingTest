0c08bf450c6db699af405981bc2a363454877de7
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-05-12 23:30:27 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(myString.toUpperCase());
+        return sb.toString();
+    }
+}
