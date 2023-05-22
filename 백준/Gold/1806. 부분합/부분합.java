0635be187a291c619d1007076bf0c9ecfc028075
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-05-22 13:12:41 +0900

    [Gold IV] Title: 부분합, Time: 288 ms, Memory: 23652 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int N, S;
+    public static int[] arr;
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i = 0 ; i < N ; i++) {
+		    arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int left = 0;
+		int right = 0;
+		
+		int sum = 0;
+		int answer = Integer.MAX_VALUE;
+		
+		while(true) {
+		    if(sum >= S) {
+		        answer = Math.min(answer, right - left);
+		        sum -= arr[left++];
+		    }
+		    else if(right == N) {
+		        break;
+		    }
+		    else {
+		        sum += arr[right++];
+		    }
+		}
+		
+		System.out.println(answer == Integer.MAX_VALUE ? 0 : answer);
+	}
+}
