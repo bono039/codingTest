cb3137fc1a0ac26a0c3b6f01a81cb1a1ecf8a849
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-05-25 23:15:22 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,61 +2,56 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    // 상하좌우 탐색 위한 배열 선언
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    static int[] dx = {0, 0, -1, 1};    // 좌우
+    static int[] dy = {1, -1, 0, 0};    // 상하
     
     static boolean[][] visited;
     static int[][] A;
     
-    static int N, M;
-    
+    static int N, M;    
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
-		N = Integer.parseInt(st.nextToken());   // row
-		M = Integer.parseInt(st.nextToken());   // col
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		A = new int[N][M];
 		visited = new boolean[N][M];
 		
+		A = new int[N][M];  // 101?
+		
+		// 입력 받기
 		for(int i = 0 ; i < N ; i++) {
-		    st = new StringTokenizer(br.readLine()," ");
-		    
-		    String line = st.nextToken();
-		    
+		    String str = br.readLine();
 		    for(int j = 0 ; j < M ; j++) {
-		        A[i][j] = Integer.parseInt(line.substring(j, j + 1));
+		        A[i][j] = str.charAt(j) - '0';
 		    }
 		}
 		
-		BFS(0, 0);
+        BFS(0, 0);
 		System.out.println(A[N - 1][M - 1]);
 	}
 	
-	// BFS 구현
-	public static void BFS(int i, int j) {
+	static void BFS(int x, int y) {
+	    visited[x][y] = true;
+	    
 	    Queue<int[]> queue = new LinkedList<>();
-	    queue.add(new int[] {i, j});
-	    visited[i][j] = true;
+	    queue.add(new int[] {x, y});
 	    
 	    while(!queue.isEmpty()) {
-	        int now[] = queue.poll();
+	        int[] now = queue.poll();
 	        
-	        for(int k = 0 ; k < 4 ; k++) {
-	            int x = now[0] + dx[k];
-	            int y = now[1] + dy[k];
+	        // 상하좌우 이동
+	        for(int d = 0 ; d < 4 ; d++) {
+	            int newX = now[0] + dx[d];
+	            int newY = now[1] + dy[d];
 	            
-	            // 좌표 유효성 검사
-	            if(x >= 0 && y >= 0 && x < N && y < M) {
-	                if(A[x][y] != 0 && !visited[x][y]) {
-	                    visited[x][y] = true;
-	                    A[x][y] = A[now[0]][now[1]] + 1;    // 깊이 업데이트
-	                    queue.add(new int[] {x,y});
-	                }
-	            }
-	        } 
+	            if(0 <= newX && newX < N && 0 <= newY && newY < M && !visited[newX][newY] && A[newX][newY]!= 0) {
+	                visited[newX][newY] = true;
+	                A[newX][newY] = A[now[0]][now[1]] + 1;  //  깊이 업데이트
+	                queue.add(new int[]{newX, newY});
+	            }       
+	        }
 	    }
 	}
 }
