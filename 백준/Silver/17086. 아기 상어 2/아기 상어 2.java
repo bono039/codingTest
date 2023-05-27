0c0f437c3e606bd3d8cd4c5bb23530edfba49220
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-05-27 22:42:32 +0900

    [Silver II] Title: 아기 상어 2, Time: 460 ms, Memory: 199908 KB -BaekjoonHub

---
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int[] dx = {0,  0, -1, 1, -1, -1, 1, 1};
+    static int[] dy = {1, -1,  0, 0, -1,  1,-1, 1};
+    
+    static int N, M, answer;
+    static int[][] map;
+    static boolean[][] visited;
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N + 1][M + 1];
+		
+		// 입력 받기
+		for(int i = 0 ; i < N ; i++) {
+		    st = new StringTokenizer(br.readLine()," ");
+		    for(int j = 0 ; j < M ; j++) {
+		        map[i][j] = Integer.parseInt(st.nextToken());
+		    }
+		}
+		
+		// 각 점마다 BFS 수행
+		for(int i = 0 ; i < N ; i++) {
+		    for(int j = 0 ; j < M ; j++) {
+		        if(map[i][j] == 1) continue;
+		        
+		        int tmp = bfs(i, j);
+    		    answer = tmp > answer ? tmp : answer;
+		    }
+		}		
+		
+		System.out.println(answer);
+	}
+	
+	static int bfs(int x, int y) {
+	    Queue<int[]> queue = new LinkedList<>();
+	    queue.add(new int[] {x, y, 0});
+	    
+	    visited = new boolean[N + 1][M + 1];
+	    visited[x][y] = true;
+	    
+	    while(!queue.isEmpty()) {
+	        int[] now = queue.poll();
+	        
+	        for(int d = 0 ; d < 8 ; d++) {
+    	        int newX = now[0] + dx[d];
+    	        int newY = now[1] + dy[d];
+    	        int newDistance = now[2] + 1;
+	            
+	            if(newX < 0 || newX >= N || newY < 0 || newY >= M) continue;
+	            if(visited[newX][newY]) continue;
+	            
+	            if(map[newX][newY] == 1) {
+	                return newDistance;
+	            }
+	            
+	            queue.offer(new int[] {newX, newY, newDistance});
+	            visited[newX][newY] = true;
+	        }
+	    }
+	    return 0;
+	}
+}
