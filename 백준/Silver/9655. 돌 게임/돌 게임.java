d84878c1deaf59a5399778f119d2e221460f14b9
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-05-31 22:40:41 +0900

    [Silver V] Title: 돌 게임, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -3,40 +3,38 @@ import java.io.*;
 
 public class Main {
     static int N;
+    static String[] rock = new String[1001];
     
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine()); 
-		String result = recursion(N, "SK");
-		System.out.println(result);
-	}
-	
-	static String recursion(int rock, String name) {
-	    if(name.equals("SK")) {
-	        if(rock - 3 >= 0) {
-	            rock -= 3;
-	            return recursion(rock, "CY");
-	        }
-	        else if(rock - 1 >= 0) {
-	            rock--;
-	            return recursion(rock, "CY");
-	        } else if(rock == 0) {
-	            return "CY";
-	        }
-	    }
-	    else if(name.equals("CY")) {
-	        if(rock - 3 >= 0) {
-	            rock -= 3;
-	            return recursion(rock, "SK");
-	        }
-	        else if(rock - 1 >= 0) {
-	            rock--;
-	            return recursion(rock, "SK");
-	        } else if(rock == 0) {
-	            return "SK";
-	        }
-	    }
-	    return "";
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        
+        if (N >= 1) {
+            rock[1] = "SK";
+        }
+        if (N >= 2) {
+            rock[2] = "CY";
+        }
+        
+        dp();
+        
+        System.out.println(rock[N]);
+    }
+    
+    static void dp() {
+        for (int cnt = 3; cnt <= N; cnt++) {
+            if (rock[cnt - 1].equals("SK") && (cnt + 3 == N || cnt + 1 == N)) {
+                rock[cnt] = "CY";
+            } else if (rock[cnt - 1].equals("CY") && (cnt + 3 == N || cnt + 1 == N)) {
+                rock[cnt] = "SK";
+            } else {
+                if (rock[cnt - 1].equals("SK")) {
+                    rock[cnt] = "CY";
+                } else if (rock[cnt - 1].equals("CY")) {
+                    rock[cnt] = "SK";
+                }
+            }
+        }
+    }
 }
