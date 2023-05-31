87df28116d68c785f216322aa87da6d2dd1c95ad
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-01 00:56:47 +0900

    [Silver I] Title: 정수 삼각형, Time: 300 ms, Memory: 26852 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int[][] A = new int[n + 1][n + 1];
+		
+		for(int i = 1 ; i <= n ; i++) {
+		    st = new StringTokenizer(br.readLine()," ");
+		    for(int j = 1 ; j <= i ; j++) {
+		        A[i][j] = Integer.parseInt(st.nextToken());
+		    }
+		}
+		
+		if(n == 1) {
+		    System.out.println(A[1][1]);
+		    return;
+		}
+		
+		int[][] dp = new int[501][501];
+		dp[1][1] = A[1][1];
+		
+		for(int i = 2 ; i <= n ; i++) {
+		    dp[i][1] = dp[i-1][1] + A[i][1];    
+		}
+		
+		int max = 0;
+		for(int i = 2 ; i <= n ; i++) {
+		    for(int j = 1 ; j <= i ; j++) {
+		        if(i - 1 < 1 || j - 1 < 1) continue;
+		        
+		        dp[i][j] = Math.max(dp[i][j], Math.max(dp[i-1][j] + A[i][j], dp[i-1][j-1] + A[i][j]));
+		        max = Math.max(max, dp[i][j]);
+		    }
+		}
+		
+		System.out.println(max);
+	}
+}
