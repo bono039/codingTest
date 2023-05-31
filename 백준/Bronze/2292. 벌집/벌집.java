4f135288ea0341e6dbe26222564f1d11f84420a0
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-05-31 16:42:43 +0900

    [Bronze II] Title: 벌집, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt = 1;
+		int range = 2;
+		
+		if(N == 1) System.out.println(1);
+		else {
+		    while(range <= N) {
+		        range = range + (6 * cnt);
+		        cnt++;
+		    }
+		    System.out.println(cnt);
+		}
+	}
+}
