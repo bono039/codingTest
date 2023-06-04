5a4005155fe4587887d76b820f0063c93108658a
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-04 22:12:48 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 18328 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] c1 = br.readLine().toCharArray();
+        char[] c2 = br.readLine().toCharArray();
+
+        int len1 = c1.length;
+        int len2 = c2.length;
+        
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        
+        for(int i = 1 ; i <= len1 ; i++) {
+            for(int j = 1 ; j <= len2 ; j++) {
+                if(c1[i - 1] == c2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        System.out.println(dp[len1][len2]);   
+    }
+}
