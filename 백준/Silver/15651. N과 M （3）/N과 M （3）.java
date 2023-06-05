eeedc0f242527a2a951899e6ea19b265ffd72535
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-05 17:56:51 +0900

    [Silver III] Title: N과 M (3), Time: 580 ms, Memory: 207332 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -4,7 +4,6 @@ import java.io.*;
 public class Main {
     static int N, M;
     static int[] A;
-    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
@@ -15,7 +14,6 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         
         A = new int[M];
-        visited = new boolean[N];
         
         dfs(0);
         System.out.println(sb);
@@ -31,10 +29,8 @@ public class Main {
         }
         
         for(int i = 0 ; i < N ; i++) {
-            visited[i] = true;
             A[depth] = i + 1;
             dfs(depth + 1);
-            visited[i] = false; 
         }
     }
 }
