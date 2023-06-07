e3427c784403ea94218948973c7732b3335f0094
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-07 17:16:59 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		
-		long sum = 0;
-		long max = 0;
-		
-		for(int i=0 ; i < N ; i++) {
-		    int tmp = sc.nextInt();
-		    
-		    if(tmp > max) max = tmp;
-		    sum += tmp;
-		}
-		
-		System.out.println(sum * 100.0 / max / N);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        double[] arr = new double[N];
+        
+        st = new StringTokenizer(br.readLine(), " ");
+        double max = 0;
+        for(int i = 0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
+        }
+        
+        double avg = 0;
+        double sum = 0;
+        for(int i = 0 ; i < N ; i++) {
+            arr[i] = arr[i] / max * 100;
+            sum += arr[i];
+        }
+        
+        System.out.println(sum / N);
+    }
 }
