07f747143a5b1a25c7437627d5dd945cb925591b
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-07 23:26:28 +0900

    [Gold IV] Title: 좋다, Time: 180 ms, Memory: 14904 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -2,48 +2,50 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-	    var br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		long[] arr = new long[N];
-		int cnt = 0;
-		
-        var st = new StringTokenizer(br.readLine()," ");
-		for(int i = 0 ; i < N ; i++) {
-		    arr[i] = Long.parseLong(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		
-		for(int k = 0 ; k < N ; k++) {
-		    long find = arr[k]; // 찾고자 하는 값
-		    int i = 0;
-		    int j = N - 1;
-		    
-		    // 투 포인터 알고리즘
-		    while(i < j) {
-		        if(arr[i] + arr[j] == find) {
-		            // 서로 다른 두 수의 합인지 확인
-		            if(i != k && j != k)  {
-		                cnt++;
-		                break;
-		            }
-		            else if(i == k) {
-		                i++;
-		            }
-		            else if(j == k) {
-		                j--;
-		            }
-		        }
-		        else if(arr[i] + arr[j] < find) {
-	                i++;
-	            } else {
-	                j--;
-	            }
-		    }
-		}
-		System.out.println(cnt);
-		br.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        long[] A = new long[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < N ; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(A);
+        
+        int cnt = 0;
+        for(int k = 0 ; k < N ; k++) {
+            long find = A[k];
+            
+            int s = 0;
+            int e = N - 1;            
+            
+            while(s < e) {
+                long sum = A[s] + A[e];
+                
+                if(sum == find) {
+                    if(s != k && e != k) {
+                        cnt++;
+                        break;
+                    }
+                    else if(s == k) {
+                        s++;
+                    }
+                    else if(e == k) {
+                        e--;
+                    }
+                }
+                else if(sum < find) {
+                    s++;
+                }
+                else if(sum > find) {
+                    e--;
+                }
+            }   
+            
+        }
+        System.out.println(cnt);
+    }
 }
