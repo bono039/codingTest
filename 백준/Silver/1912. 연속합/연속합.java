5cb229d3b4393d53735225f8c4d73eb94b7bf540
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-09 23:16:10 +0900

    [Silver II] Title: 연속합, Time: 268 ms, Memory: 24244 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -2,31 +2,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
-        String[] str = br.readLine().split(" ");
-        int[] A = new int[100001];
-        
-        for (int i = 0; i < n; i++) {
-            A[i + 1] = Integer.parseInt(str[i]);
-        }
-        
-        long[] S = new long[100001];
-        S[1] = A[1];
-        for (int i = 2; i <= n; i++) {
-            if (S[i - 1] + A[i] < A[i]) {
-                S[i] = A[i];
-            } else {
-                S[i] = A[i] + S[i - 1];
-            }
-        }
-        
-        long answer = -1000;
-        for (int i = 1; i <= n; i++) {
-            answer = Math.max(answer, S[i]);
-        }
-        System.out.println(answer);
-    }
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] A = new int[n + 1];
+		
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i = 1 ; i <= n ; i++) {
+		    A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+	    long[] S = new long[100_001];
+		S[1] = A[1];
+		
+		for(int i = 2 ; i <= n ; i++) {
+		    if(S[i-1] + A[i] < A[i]) {
+		        S[i] = A[i];
+		    } else {
+		        S[i] = A[i] + S[i - 1];
+		    }
+		}
+		
+		long answer = -1000;
+		for(int i = n ; i > 0 ; i--) {
+		    answer = Math.max(answer, S[i]);
+		}
+		System.out.println(answer);
+	}
 }
