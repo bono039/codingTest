c7633512fd51cec4195fc939c9691f396096d379
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-09 16:23:39 +0900

    [Bronze II] Title: 수 이어 쓰기 3, Time: 164 ms, Memory: 16604 KB -BaekjoonHub

---
+++ 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 1 ; i <= N ; i++) {
+            sb.append(i);
+        }
+        
+        System.out.println(sb.indexOf(Integer.toString(N)) + 1);
+    }
+}
