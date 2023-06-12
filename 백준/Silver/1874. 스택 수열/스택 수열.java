64b3aa0a7159f3680d23e3035717b88ca16c4e3d
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-12 23:27:03 +0900

    [Silver II] Title: 스택 수열, Time: 348 ms, Memory: 26760 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,33 +2,45 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] A = new int[n];
+		for(int i = 0 ; i < n ; i++) {
+		    A[i] = Integer.parseInt(br.readLine());
+		}
+		
         Stack<Integer> stack = new Stack<>();
-        int n = Integer.parseInt(br.readLine());
-        int start = 0;
+        StringBuilder sb = new StringBuilder();
+        int num = 1;
+        boolean result = true;
         
-        while(n --> 0) {
-            int value = Integer.parseInt(br.readLine());
-            
-            if(value > start) {
-                for(int i = start + 1 ; i <= value; i ++) {
-                    stack.push(i);
-                    sb.append('+').append("\n");
-                }
-                start = value;
-            }
-            else if(stack.peek() != value) {
-                System.out.println("NO");
-                return;
-            }
-            
-            stack.pop();
-            sb.append("-").append("\n");
-        }
-        
-        System.out.println(sb);
-    }
+		for(int i = 0 ; i < n ; i++) {
+		    int cur = A[i];
+		    
+		    if(cur >= num) {    
+		        while(cur >= num) {
+		            stack.push(num++);
+		            sb.append("+\n");
+		        }
+		        stack.pop();
+		        sb.append("-\n");
+		    }
+		    else {
+		        int now = stack.pop();
+		        
+		        if(now > cur) {
+		            System.out.println("NO");
+		            result = false;
+		            break;
+		        }
+		        else {
+		            sb.append("-\n");
+		        }
+		    }
+		}
+		
+		if(result) System.out.println(sb.toString());
+	}
 }
