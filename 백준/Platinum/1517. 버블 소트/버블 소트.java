48a170de0756b0a51babc7c8d823381ff93cbea7
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-14 18:19:09 +0900

    [Platinum V] Title: 버블 소트, Time: 568 ms, Memory: 78060 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.java
+++ 백준/Platinum/1517. 버블 소트/버블 소트.java
@@ -2,66 +2,61 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static long[] A;
-    static long[] tmp;
-    static long result;
+    static long[] A, tmp;
+    static long cnt;
     
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    
-	    int N = Integer.parseInt(br.readLine());
-	    A = new long[N + 1];
-	    tmp = new long[N + 1];
-	    
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int N = Integer.parseInt(br.readLine());
+		A  = new long[N + 1];
+		tmp  = new long[N + 1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 1 ; i <= N ; i++) {
-		    A[i] = Integer.parseInt(st.nextToken());
+		    A[i] = Long.parseLong(st.nextToken());
 		}
 		
-		result = 0;
-		merge_sort(1, N);   // 병합 정렬
-		
-		System.out.println(result);
+		cnt = 0;
+		merge_sort(1, N);
+		System.out.println(cnt);
 	}
 	
-	public static void merge_sort(int s, int e) {
-	    if(e - s < 1)
-	        return;
-	   
-	    int mid = s + (e - s) / 2;
-	    merge_sort(s, mid);
-	    merge_sort(mid + 1 , e);
-	    
-	    // tmp 배열 저장
+	private static void merge_sort(int s, int e) {
+	    if(s < e) {
+	        int mid = s + (e - s) / 2;
+	        merge_sort(s, mid);
+	        merge_sort(mid + 1, e);
+	        merge(s, mid, e);
+	    }
+	}
+	
+	private static void merge(int s, int m, int e) {
 	    for(int i = s ; i <= e ; i++) {
 	        tmp[i] = A[i];
 	    }
 	    
-	    // 두 그룹 병합
-	    int tmpIdx = s; // 현재 복사 위치
-	    int idx1 = s;
-	    int idx2 = mid + 1;
+	    int l = s;
+	    int r = m + 1;
+	    int idx = s;
 	    
-	    while(idx1 <= mid && idx2 <= e) {
-	        if(tmp[idx1] > tmp[idx2]) {
-	            // 뒤쪽 데이터 값이 작은 경우 업데이트
-	            A[tmpIdx] = tmp[idx2];
-	            result += idx2 - tmpIdx;
-	            tmpIdx++;
-	            idx2++;
+	    while(l <= m && r <= e) {
+	        if(tmp[l] <= tmp[r]) {
+	            A[idx] = tmp[l];
+	            l++;
+	        } else {
+	            A[idx] = tmp[r];
+	            cnt += r - idx;
+	            r++;
 	        }
-	        else {
-	            A[tmpIdx++] = tmp[idx1++];
-	        }
+	        idx++;
 	    }
 	    
-	    
-	    // 부분이 남은 경우
-	    while(idx1 <= mid) {
-	        A[tmpIdx++] = tmp[idx1++];
+	    while(l <= m) {
+	        A[idx++] = tmp[l++];
 	    }
-	    while(idx2 <= e) {
-	        A[tmpIdx++] = tmp[idx2++];
+	    while(r <= e) {
+	        A[idx++] = tmp[r++];
 	    }
 	}
 }
