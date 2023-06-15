ae078b16c85696da3c9ec783f63f41b0e31b33e7
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-16 00:10:23 +0900

    [Gold II] Title: 트리의 지름, Time: 840 ms, Memory: 93600 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -2,87 +2,80 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static boolean[] visited;   // 방문 배열
-    static int[] distance;      // 거리 저장 배열
-    static ArrayList<Edge>[] A; // 그래프 데이터 저장 인접 리스트    
+    static int V;
+    static ArrayList<Node>[] A;
+    static boolean[] visited;
+
+    static int[] dp;
     
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());   // 정점 개수
-		A = new ArrayList[N + 1];
-		for(int i = 1 ; i <= N ; i++) {
-		    A[i] = new ArrayList<Edge>();
+		V = Integer.parseInt(br.readLine());
+		
+		A = new ArrayList[V + 1];
+		for(int i = 1 ; i <= V ; i++) {
+		    A[i] = new ArrayList<Node>();
 		}
 		
-		// 인접 리스트에 그래프 데이터 저장
-		for(int i = 0 ; i < N ; i++) {
-		    st = new StringTokenizer(br.readLine()," ");
+		for(int i = 1 ; i <= V ; i++) {
+		    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		    
-		    int S = Integer.parseInt(st.nextToken());
+		    int s = Integer.parseInt(st.nextToken());
 		    
 		    while(true) {
-		        int E = Integer.parseInt(st.nextToken());
+		        int e = Integer.parseInt(st.nextToken());
+		        if(e == -1) break;
 		        
-		        if(E == -1) break;
-		        
-		        int V = Integer.parseInt(st.nextToken());
-		        A[S].add(new Edge(E, V));
+                int v = Integer.parseInt(st.nextToken());
+		        A[s].add(new Node(e, v));
 		    }
 		}
 		
-		distance = new int[N + 1];
-		visited = new boolean[N + 1];
+		visited = new boolean[V + 1];
+		dp = new int[V + 1];
+		bfs(1);
 		
-		BFS(1);
-		
-		// distance 배열에서 가장 큰 값으로 다시 시작점 설정
 		int max = 1;
-		for(int i = 2 ; i <= N ; i++) {
-		    if(distance[max] < distance[i])
+		for(int i = 2 ; i <= V ; i++) {
+		    if(dp[max] < dp[i]) {
 		        max = i;
+		    }
 		}
 		
-		distance = new int[N + 1];
-		visited = new boolean[N + 1];
-		BFS(max);   // 새로운 시작점으로 실행
+		visited = new boolean[V + 1];
+		dp = new int[V + 1];
+		bfs(max);
 		
-		Arrays.sort(distance);
-		System.out.println(distance[N]);
+		Arrays.sort(dp);
+		System.out.println(dp[V]);
 	}
 	
-	// BFS 구현
-	private static void BFS(int idx) {
+	private static void bfs(int num) {
 	    Queue<Integer> queue = new LinkedList<>();
-	    queue.add(idx);
-	    visited[idx] = true;
+	    queue.add(num);
+	    visited[num] = true;
 	    
 	    while(!queue.isEmpty()) {
 	        int now = queue.poll();
 	        
-	        for(Edge i : A[now]) {
-	            
-	            int e = i.e;
-	            int v = i.val;
-	            
-	            if(!visited[e]) {
-	                visited[e] = true;
-	                queue.add(e);
-	                distance[e] = distance[now] + v;    // 노드 거리 배열 업데이트 (현재 노드 거리 + 에지 가중치)
+	        for(Node next : A[now]) {
+	            if(!visited[next.idx]) {
+	                visited[next.idx] = true;
+	                queue.add(next.idx);
+	                dp[next.idx] = dp[now] + next.val;
 	            }
 	        }
 	    }
 	}
 }
 
-	
-class Edge {
-    int e;      // 목적지 노드
-    int val;    // 엣지 가중치
+class Node {
+    int idx;
+    int val;
     
-    public Edge(int e, int val) {
-        this.e = e;
+    Node(int idx, int val) {
+        this.idx = idx;
         this.val = val;
     }
 }
