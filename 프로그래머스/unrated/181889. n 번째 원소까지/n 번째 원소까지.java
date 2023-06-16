bf5a25dba4ce6e729cc5a1b166ebf29ca0235afe
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-16 18:17:15 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[n];
+        
+        for(int i = 1 ; i <= n ; i++) {
+            answer[i-1] = num_list[i-1];
+        }
+        
+        return answer;
+    }
+}
