a7be12a236190248fcecd718799e7a6f163b4e47
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-22 15:48:36 +0900

    [Gold IV] Title: 여행 가자, Time: 188 ms, Memory: 16956 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -12,20 +12,14 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		N = Integer.parseInt(br.readLine());    // 도시 수
-		M = Integer.parseInt(br.readLine());    // 여행 계획에 속한 도시들 수
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
 		
-		// 각종 배열, 변수 초기화하기
-
 		parent = new int[N + 1];
 		for(int i = 1 ; i <= N ; i++) {
 		    parent[i] = i;
 		}
-
-		isPossible = true;
 		
-		
-		// N개 줄 연결 정보 입력 받기
 		map = new int[N + 1][N + 1];
 		for(int i = 1 ; i <= N ; i++) {
 		    st = new StringTokenizer(br.readLine(), " ");
@@ -37,18 +31,14 @@ public class Main {
 		        }
 		    }
 		}
-
 		
-		// 여행 계획 입력 받기
 		plan = new int[M + 1];
 		st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 1 ; i <= M ; i++) {
 		    plan[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		
-		// 여행 계획에서 find해서 모두 같은 집합일 때 (true) => YES
-		// 조합?
+		isPossible = true;
 		for(int i = 1 ; i < plan.length ; i++) {
 		    if(find(plan[i]) != find(plan[1])) {
 		        isPossible = false;
