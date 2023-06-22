70ed985d47fad6d90872dd293675855af4a1540a
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-22 14:57:14 +0900

    [Gold V] Title: 집합의 표현, Time: 676 ms, Memory: 57248 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,64 +1,48 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
     static int[] parent;
     
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
 		
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		parent = new int[n + 1];
 		
-		// 배열 채우기
+		parent = new int[n + 1];
 		for(int i = 1 ; i <= n ; i++) {
 		    parent[i] = i;
 		}
 		
-		// 입력 받기
-		for(int i = 0 ; i < m ; i++) {
-		    st = new StringTokenizer(br.readLine()," ");
+		while(m --> 0) {
+		    st = new StringTokenizer(br.readLine(), " ");
 		    
-		    int uf = Integer.parseInt(st.nextToken());
+		    int num = Integer.parseInt(st.nextToken());
 		    int a = Integer.parseInt(st.nextToken());
 		    int b = Integer.parseInt(st.nextToken());
 		    
-		    // UNION 진행
-		    if(uf == 0) {
+		    if(num == 0) {
 		        union(a, b);
 		    }
 		    else {
-		        if(checkSame(a, b)) System.out.println("YES");
-		        else                System.out.println("NO");
+		        sb.append(find(a) == find(b) ? "YES" : "NO").append("\n");
 		    }
 		}
+		System.out.println(sb);
 	}
 	
-	// UNION
-	public static void union(int a, int b) {
+	static void union(int a, int b) {
 	    a = find(a);
 	    b = find(b);
 	    
-	    if(a != b) {
-	        parent[b] = a;
-	    }
+	    if(a != b)  parent[b] = a;
 	}
 	
-	// FIND
-	public static int find(int a) {
+	static int find(int a) {
 	    if(a == parent[a])  return a;
-	    else                return parent[a] = find(parent[a]); // 경로 압축
-	}
-	
-	// 두 원소가 같은 집합인지 확인
-	public static boolean checkSame(int a, int b) {
-	    a = find(a);
-	    b = find(b);
-	    
-	    if(a == b) {
-	        return true;
-	    }
-	    return false;
+	    return parent[a] = find(parent[a]);
 	}
 }
