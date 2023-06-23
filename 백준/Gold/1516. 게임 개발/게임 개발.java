84329c6c3d26b783d96a5b642eaa651663a4a40b
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-23 16:51:06 +0900

    [Gold III] Title: 게임 개발, Time: 272 ms, Memory: 21624 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -3,58 +3,60 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		ArrayList<ArrayList<Integer>> A = new ArrayList<>();   
-		for(int i = 0 ; i <= N ; i++) {
-		    A.add(new ArrayList<>());
+		ArrayList<Integer>[] A = new ArrayList[N + 1];  // 인접 리스트
+		for(int i = 1 ; i <= N ; i++) {
+		    A[i] = new ArrayList<>();
 		}
 		
 		int[] indegree = new int[N + 1];    // 진입 차수 배열
-		int[] time = new int[N + 1];    // 건물 생산 시간
+		int[] time = new int[N + 1];        // 건물 생산 시간
 		
 		for(int i = 1 ; i <= N ; i++) {
-		    StringTokenizer st = new StringTokenizer(br.readLine());
+		    st = new StringTokenizer(br.readLine(), " ");
 		    time[i] = Integer.parseInt(st.nextToken());
 		    
-		    // 인접 리스트 초기화
 		    while(true) {
-		        int preTemp = Integer.parseInt(st.nextToken());
-		        if(preTemp == -1) break;
-		        A.get(preTemp).add(i);
+		        int idx = Integer.parseInt(st.nextToken());
+		        if(idx == -1)   break;
+		        A[idx].add(i);
 		        indegree[i]++;
 		    }
 		}
 		
-		
 		// 위상 정렬
 		Queue<Integer> queue = new LinkedList<>();
 		for(int i = 1 ; i <= N ; i++) {
-		    if(indegree[i] == 0) {
-		        queue.offer(i);
-		    }
+	        if(indegree[i] == 0) {
+	            queue.add(i);
+	        }
 		}
-		
-		int[] result = new int[N + 1];  // 정답 리스트
+        
+        int[] result = new int[N + 1];  // 정답 리스트
+        
 		while(!queue.isEmpty()) {
 		    int now = queue.poll();
 		    
-		    for(int next : A.get(now)) {
+		    for(int next : A[now]) {
 		        indegree[next]--;
 		        
-		        // 시간 업데이트 : Math.max(현재 저장된 값, 현재 출발 노드 + 비용)
+		        // 시간 업데이트
 		        result[next] = Math.max(result[next], result[now] + time[now]);
 		        
 		        if(indegree[next] == 0) {
-		            queue.offer(next);
+		            queue.add(next);
 		        }
 		    }
 		}
-
+		
 		for(int i = 1 ; i <= N ; i++) {
-		    System.out.println(result[i] + time[i]);
+		    sb.append(result[i] + time[i]).append("\n");
 		}
+		System.out.println(sb);
 	}
 }
