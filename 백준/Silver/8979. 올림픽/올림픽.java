e7c761c31a23f66cceae292f55f4f0fddb5d1b2b
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-24 01:23:52 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+//금은동 배열 3개 만들면 넘 비효율적일 것 같아,,,,,
+// 아님 3/4차원 배열 만들어버려?ㅎ 이것도 아닌 것 같다
+
+public class Main {
+    static int N, K;
+    static ArrayList<Node> list = new ArrayList<>();
+    static int endPoint = 0;
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());   // 국가 수
+		K = Integer.parseInt(st.nextToken());   // 등수 알고 싶은 국가
+
+		// 입력 받기
+		while(N --> 0) {
+		    st = new StringTokenizer(br.readLine(), " ");
+		    int num = Integer.parseInt(st.nextToken());
+		    int gold = Integer.parseInt(st.nextToken());
+		    int silver = Integer.parseInt(st.nextToken());
+		    int bronze = Integer.parseInt(st.nextToken());
+		    
+		    list.add(new Node(num, gold, silver, bronze, 0));
+		}
+		
+		Collections.sort(list);
+		list.get(0).rank = 1;    // 정렬 후, 리스트의 첫 번째 국가는 1등
+		
+		// 등수 매겨주기
+        for(int i = 1 ; i < list.size() ; i++) {
+            Node now = list.get(i);
+            
+            int tmpG = list.get(i - 1).g;
+            int tmpS = list.get(i - 1).s;
+            int tmpB = list.get(i - 1).b;
+            
+            // 국가 번호가 K면, endPoint (마지막 정답 출력 시 사용)
+            if(list.get(i).num == K) {
+                endPoint = i;
+            }
+            
+            if(now.g == tmpG && now.s == tmpS && now.b == tmpB) {
+                list.get(i).rank = list.get(i - 1).rank;
+            }
+            else {
+                list.get(i).rank = i + 1;
+            }
+        }
+		
+		
+		// 찾는 등수 원소 구하기
+		System.out.println(list.get(endPoint).rank - 1);
+	}
+}
+
+class Node implements Comparable<Node>{
+    int num, g, s, b, rank;
+    
+    Node(int num, int g, int s, int b, int rank) {
+        this.num = num;
+        this.g = g;
+        this.s = s;
+        this.b = b;
+        this.rank = rank;
+    }
+    
+    // 금메달 순 순위 결정 (내림차순)
+    @Override
+    public int compareTo(Node n) {
+        if(this.g == n.g) {
+            if(this.s == this.s) {
+                return n.b - this.b;    // 내림차순
+            }
+            else {
+                return n.s - this.s;
+            }
+        }
+        else {
+            return n.g - this.g;
+        }
+    }
+}
