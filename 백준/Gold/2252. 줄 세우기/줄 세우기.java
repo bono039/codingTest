2ff5c604be617c6aaa557fb7e09fc932c2bf8763
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-23 15:08:56 +0900

    [Gold III] Title: 줄 세우기, Time: 524 ms, Memory: 50936 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -3,50 +3,49 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());   // 학생 수
+		int M = Integer.parseInt(st.nextToken());   // 키 비교 횟수
 		
-		ArrayList<ArrayList<Integer>> A = new ArrayList<>();
-		for(int i = 0 ; i <= N ; i++) {
-		    A.add(new ArrayList<>());
-		}
-		
-		int[] indegree = new int[N + 1];   // 진입 차수 배열
-		while(M --> 0) {
-		    st = new StringTokenizer(br.readLine()," ");
-		    int S = Integer.parseInt(st.nextToken());
-		    int E = Integer.parseInt(st.nextToken());
-		    
-		    A.get(S).add(E);
-		    indegree[E]++;
-		}
-		
-		// 위상 정렬 수행
-		Queue<Integer> queue = new LinkedList<>();
+		ArrayList<Integer>[] A = new ArrayList[N + 1];
 		for(int i = 1 ; i <= N ; i++) {
-		    // 1. 진입 차수가 0인 노드 큐에 저장
+		    A[i] = new ArrayList<>();
+		}
+		
+		// 키 정보 입력받기
+		int[] indegree = new int[N + 1];    // 진입 차수 배열
+		while(M --> 0) {
+		    st = new StringTokenizer(br.readLine(), " ");
+		    int a = Integer.parseInt(st.nextToken());
+		    int b = Integer.parseInt(st.nextToken());
+		    
+		    A[a].add(b);    // 단방향
+		    indegree[b]++;
+		}
+		
+		Queue<Integer> queue = new LinkedList<>(); // 정렬 리스트
+		for(int i = 1 ; i <= N ; i++) {
 		    if(indegree[i] == 0) {
 		        queue.offer(i);
 		    }
 		}
 		
 		while(!queue.isEmpty()) {
-		    // 2-1. 큐에서 데이터 poll해 해당 노드를 탐색 결과에 추가
 		    int now = queue.poll();
-		    System.out.print(now + " ");
+		    sb.append(now).append(" ");		    
 		    
-		    for(int target : A.get(now)) {
-		        // 2-2. 해당 노드가 가리키는 노드의 진입 차 수 1씩 감소 
-		        indegree[target]--;
+		    for(int next : A[now]) {
+		        indegree[next]--;
 		        
-		        // 3. 감소했을 때 진입 차수가 0이 되는 노드를 큐에 추가
-		        if(indegree[target] == 0) {
-		            queue.offer(target);
+		        if(indegree[next] == 0) {
+		            queue.offer(next);
 		        }
 		    }
 		}
+		
+		System.out.println(sb);
 	}
 }
