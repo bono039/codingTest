08bd9309419251bf0e2deeecef58515a161f0e8f
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-26 23:41:12 +0900

    [Gold IV] Title: 플로이드, Time: 464 ms, Memory: 42960 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -2,48 +2,52 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M;
-    static int[][] distance;
-    static int max = 10000001;
+    static int N,M;
+    static int[][] dp;  // 최단 거리 배열
+    static int INF = 987654321;
     
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());    // 도시 개수
+		M = Integer.parseInt(br.readLine());    // 버스 개수
 		
-		distance = new int[N + 1][N + 1];
+		// 최단 거리 배열 초기화
+		dp = new int[N + 1][N + 1];
 		for(int i = 1 ; i <= N ; i++) {
 		    for(int j = 1 ; j <= N ; j++) {
-		        if(i == j) distance[i][j] = 0;
-		        else       distance[i][j] = max;
+		        dp[i][j] = (i == j) ? 0 : INF;
 		    }
 		}
 		
-		for(int i = 1 ; i <= M ; i++) {
-		    StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		    int S = Integer.parseInt(st.nextToken());
-		    int E = Integer.parseInt(st.nextToken());
-		    int K = Integer.parseInt(st.nextToken());
+		// 입력 받아 그래프 데이터 채우기
+		while(M --> 0) {
+		    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		    
-		    if(distance[S][E] > K) distance[S][E] = K;
+		    int s = Integer.parseInt(st.nextToken());
+		    int e = Integer.parseInt(st.nextToken());
+		    int v = Integer.parseInt(st.nextToken());
+		    
+		    dp[s][e] = Math.min(dp[s][e], v);
 		}
 		
+		// 플로이드-워셜 수행
 		for(int K = 1 ; K <= N ; K++) {
-		    for(int i = 1 ; i <= N ; i++) {
-		        for(int j = 1 ; j <= N ; j++) {
-                    if(distance[i][j] > distance[i][K] + distance[K][j])
-                        distance[i][j] = distance[i][K] + distance[K][j];
+		    for(int S = 1 ; S <= N ; S++) {
+		        for(int E = 1 ; E <= N ; E++) {
+		            dp[S][E] = Math.min(dp[S][E], dp[S][K] + dp[K][E]);
 		        }
-		    }
+		    }   
 		}
 		
+		// 출력하기
 		for(int i = 1 ; i <= N ; i++) {
 		    for(int j = 1 ; j <= N ; j++) {
-		        if(distance[i][j] == max) System.out.print("0 ");
-		        else                           System.out.print(distance[i][j] + " ");
+		        sb.append(dp[i][j] == INF ? 0 : dp[i][j]).append(" ");
 		    }
-		    System.out.println();
+		    sb.append("\n");
 		}
+		System.out.println(sb);
 	}
 }
