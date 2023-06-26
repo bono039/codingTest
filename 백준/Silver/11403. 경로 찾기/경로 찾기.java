d8928d7e1ba08193c0e47bd726ecdcc2c1f1cbf8
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-06-27 01:33:43 +0900

    [Silver I] Title: 경로 찾기, Time: 188 ms, Memory: 16496 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -3,34 +3,39 @@ import java.io.*;
 
 public class Main {
     static int N;
-    static int[][] distance;
+    static int[][] dp;
     
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
-		int N = Integer.parseInt(br.readLine());
-		distance = new int[N + 1][N + 1];
-		
-		for(int i = 1 ; i <= N ; i++) {
-		    StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		    for(int j = 1 ; j <= N ; j++) {
-		        distance[i][j] = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(br.readLine());
+        
+		dp = new int[N][N];
+		for(int i = 0 ; i < N ; i++) {
+		    st = new StringTokenizer(br.readLine(), " ");
+		    for(int j = 0 ; j < N ; j++) {
+		        dp[i][j] = Integer.parseInt(st.nextToken());
 		    }
 		}
-		
-		for(int k = 1 ; k <= N ; k++) {
-		    for(int i = 1 ; i <= N ; i++) {
-		        for(int j = 1 ; j <= N ; j++) {
-		            if(distance[i][k] == 1 && distance[k][j] == 1)  distance[i][j] = 1;
-		        }
+
+        for(int k = 0 ; k < N ; k++) {
+            for(int i = 0 ; i < N ; i++) {
+                for(int j = 0 ; j < N ; j++) {
+                    if(dp[i][k] == 1 && dp[k][j] == 1) {
+                        dp[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+		for(int i = 0 ; i < N ; i++) {
+		    for(int j = 0 ; j < N ; j++) {
+		        sb.append(dp[i][j]).append(" ");
 		    }
+		    sb.append("\n");
 		}
-		
-		for(int i = 1 ; i <= N ; i++) {
-		    for(int j = 1 ; j <= N ; j++) {
-		        System.out.print(distance[i][j] + " ");
-		    }
-		    System.out.println();
-		}
+		System.out.println(sb);
 	}
 }
