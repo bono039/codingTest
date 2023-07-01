36d638c93d6b61bca320103f840107258534a522
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-07-01 17:16:17 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+
+		for(int i = 1 ; i <= N ; i++) {
+		    sb.append(" ".repeat(i - 1));
+		    sb.append("*".repeat(2*(N - i) + 1));
+		    sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
