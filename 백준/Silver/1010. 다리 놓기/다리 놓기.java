035ade9f2e9ed055ea3ae8bb54195b15fe1ed24d
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-07-03 20:44:27 +0900

    [Silver V] Title: 다리 놓기, Time: 132 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -2,35 +2,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		long[][] bridge = new long[31][31];
-		for(int i = 0 ; i <= 30 ; i++) {
-		    bridge[i][0] = 1;
-		    bridge[i][1] = i;
-		    bridge[i][i] = 1;
-		}
-		
-		for(int i = 2 ; i <= 30 ; i++) {
-		    for(int j = 1 ; j < i ; j++) {
-		        bridge[i][j] = bridge[i - 1][j] + bridge[i - 1][j - 1];
-		    }
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		
-		while(T --> 0) {
-		    st = new StringTokenizer(br.readLine()," ");
-        	int N = Integer.parseInt(st.nextToken());   // 서
-        	int M = Integer.parseInt(st.nextToken());   // 동
-        	
-        	sb.append(bridge[M][N] + "\n");
-		}
-		
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[31][31];
+        for(int i = 0 ; i < 31 ; i++) {
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+            dp[i][1] = i;
+        }
+
+        for(int i = 1 ; i < 31 ; i++) {
+            for(int j = 2 ; j < 31 ; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+            }
+        } 
+        
+        while(T --> 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            
+            sb.append(dp[M][N]).append("\n");
+        }
         System.out.println(sb);
-        br.close();
-	}
+    }
 }
