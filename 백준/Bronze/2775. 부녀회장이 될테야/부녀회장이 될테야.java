46c52789b47382b2f53dd4452a7fa39948c715b6
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-07-03 18:44:18 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 112 ms, Memory: 13944 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,36 +1,31 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    static int T, K, N;
-    static int[][] D;
-    
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		T = Integer.parseInt(br.readLine());
-		
-		D = new int[15][15];
-		for(int i = 0 ; i < 15 ; i++) {
-		    D[i][1] = 1;
-		    D[0][i] = i;
-		}
-		
-		for(int i = 1 ; i < 15 ; i++) {
-		    for(int j = 2 ; j < 15 ; j++) {
-		        D[i][j] = D[i][j - 1] + D[i - 1][j];
-		    }
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0 ; i < T ; i++) {
-		     int K = Integer.parseInt(br.readLine());
-		     int N = Integer.parseInt(br.readLine());
-		     
-		     sb.append(D[K][N] + "\n");
-		}
-		
-		System.out.println(sb.toString());
-		br.close();
-	}
+public class Main { 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        int[][] dp  = new int[15][15];
+        for(int i = 0 ; i < 15 ; i++) {
+            dp[0][i] = i;
+            dp[i][1] = 1;
+        }
+
+        for(int i = 1 ; i < 15 ; i++) {
+            for(int j = 2 ; j < 15 ; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }        
+
+        while(T --> 0) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[k][n]).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
 }
