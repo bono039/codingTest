1e24fd2b6bb0905a60eb2f55d3cee781f44dee8c
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-07-07 23:48:04 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 336 ms, Memory: 29272 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -2,34 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		long[][] DP = new long[n + 1][m + 1];
-		for(int i = 1 ; i <= n ; i++) {
-		    String[] str = br.readLine().split("");
-		    for(int j = 1 ; j <= m ; j++) {
-		        DP[i][j] = Long.parseLong(str[j - 1]);
-		    }
-		}
-		
-		long max = 0;
-
-        for(int i = 1 ; i <= n ; i++) {
-            for(int j = 1 ; j <= m ; j++) {
-                if(DP[i][j] == 1 && i > 0 && j > 0) {
-                    DP[i][j] = Math.min(Math.min(DP[i - 1][j - 1], DP[i][j - 1]), DP[i - 1][j]) + 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        long[][] dp = new long[n][m];
+        for(int i = 0 ; i < n ; i++) {
+            char[] ch = br.readLine().toCharArray();
+            for(int j = 0 ; j < m ; j++) {
+                dp[i][j] = ch[j] - '0';
+            }
+        }
+        
+        long max = 0;
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < m ; j++) {
+                if(dp[i][j] == 1 && i > 0 && j > 0) {
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + dp[i][j];
                 }
-                if(max < DP[i][j]) {
-                    max = DP[i][j];
-                } 
+                if(max < dp[i][j]) {
+                    max = dp[i][j];
+                }
             }
         }
         System.out.println(max * max);
-		br.close();
-	}
+    }
 }
