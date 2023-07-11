7c424773a80252702784652ff1bdc6003706f19b
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-07-11 14:16:41 +0900

    [Gold V] Title: CCW, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.java
+++ 백준/Gold/11758. CCW/CCW.java
@@ -2,25 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		   
-		int x1 = Integer.parseInt(st.nextToken());
-		int y1 = Integer.parseInt(st.nextToken());
-		
-        st = new StringTokenizer(br.readLine()," ");   
-		int x2 = Integer.parseInt(st.nextToken());
-		int y2 = Integer.parseInt(st.nextToken());
-		
-        st = new StringTokenizer(br.readLine()," ");   
-		int x3 = Integer.parseInt(st.nextToken());
-		int y3 = Integer.parseInt(st.nextToken());
-		
-        int ccw = (x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         
-        if(ccw < 0)       System.out.println(-1);
-        else if(ccw == 0) System.out.println(0);
-        else              System.out.println(1);
-	}
+        int[][] pos = new int[3][2];
+        
+        for(int i = 0 ; i < 3 ; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            pos[i][0] = Integer.parseInt(st.nextToken());
+            pos[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        long tmp1 = (pos[0][0] * pos[1][1] + pos[1][0] * pos[2][1] + pos[2][0] * pos[0][1]);
+        long tmp2 = (pos[1][0] * pos[0][1] + pos[2][0] * pos[1][1] + pos[0][0] * pos[2][1]);
+        
+        long ccw = tmp1 - tmp2;
+        long answer = 0;
+        
+        if(ccw > 0) answer = 1;
+        else if(ccw < 0) answer = -1;
+        
+        System.out.println(answer);
+    }  
 }
