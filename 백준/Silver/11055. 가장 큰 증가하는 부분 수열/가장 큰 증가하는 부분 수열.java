7fe4a893e9c33a55a769384af98a5c24c33696eb
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-07-19 23:42:48 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 144 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,36 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        int[] cost = new int[N];
-        int[] dp = new int[N];
-        
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0 ; i < N ; i++) {
-            cost[i] = Integer.parseInt(st.nextToken());
-        }
-
-        dp[0] = cost[0];
-        for(int i = 1 ; i < N ; i++) {
-            dp[i] = cost[i];
-            
-            for(int j = 0 ; j < i ; j++) {
-                if(cost[i] > cost[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + cost[i]);
-                }
-            }
-        }
-        
-        int max = Integer.MIN_VALUE;
-        for(int i = 0 ; i < N ; i++) {
-            if(dp[i] > max) {
-                max = dp[i];
-            }
-        }
-        System.out.println(max);
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(reader.readLine());
+		
+		int[] map = new int[n+1];
+		int[] dp = new int[n+1];
+		
+		StringTokenizer st = new StringTokenizer(reader.readLine());
+		for (int i=1; i<=n; i++) {
+			map[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		//dp : 각 원소의 가장 큰 증가 부분 수열 합
+		//점화식 : dp[i] = max(dp[i], dp[i-1] + map[i]
+		
+		dp[1] = map[1];
+		
+		int result = dp[1];
+		for (int i=2; i<=n; i++) {
+			dp[i] = map[i];
+			for (int j=1; j<i; j++) {
+				if (map[i] > map[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + map[i]);
+				}
+			}
+			result = Math.max(result, dp[i]);
+		}
+		
+		System.out.println(result);
+	}
 }
