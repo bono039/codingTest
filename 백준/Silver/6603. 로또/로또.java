7f1f50fdeb8f8f628c5ec7f612a0f1b0b11f68b5
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-08-27 00:17:57 +0900

    [Silver II] Title: 로또, Time: 128 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,42 +1,53 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int k;
-    static int [] s;
-    static boolean [] chk;
-    public static void main(String[] args) throws  Exception {
+    static int N;
+    static int[] A;
+    static boolean[] check;
+    
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while(true){
-            String testCase=br.readLine();
-            if(testCase.equals("0")) break;
-            String [] input=testCase.split(" ");
-            k=Integer.parseInt(input[0]);
-            s=new int[k];
-            chk=new boolean[k];
-            for(int i=0;i<k;i++){
-                s[i]=Integer.parseInt(input[i+1]);
-            } //초기 값 세팅
-
-            dfs(0,0);
-            System.out.println();
+        
+        while(true) {
+            String str = br.readLine();
+            
+            if(str.equals("0")) {
+                System.out.println(sb);
+                break;
+            }
+            
+            // 초기값 세팅
+            String[] input = str.split(" ");
+            N = Integer.parseInt(input[0]);
+            A = new int[N];
+            check = new boolean[N];
+            
+            for(int i = 0 ; i < N ; i++) {
+                A[i] = Integer.parseInt(input[i + 1]);
+            }
+            
+            back(0, 0);
+            sb.append("\n");
         }
     }
-
-    public static void dfs(int depth,int start){
-        if(depth==6){
-            for(int i=0;i<k;i++){
-                if(chk[i]){
-                    System.out.print(s[i]+" ");
+    
+    private static void back(int startIdx, int depth) {
+        if(depth == 6) {
+            for(int i = 0 ; i < N ; i++) {
+                if(check[i]) {
+                    sb.append(A[i]).append(" ");
                 }
             }
-            System.out.println();
+            sb.append("\n");
         }
-        for(int i=start;i<k;i++){
-            chk[i]=true;
-            dfs(depth+1,i+1);
-            chk[i]=false;
+        
+        for(int i = startIdx ; i < N ; i++) {
+            check[i] = true;
+            back(i + 1 , depth + 1);
+            check[i] = false;
         }
     }
 }
