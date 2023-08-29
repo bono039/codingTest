94d4b4073967124326f0f0a5c13da04c021c1834
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-08-29 19:14:21 +0900

    [Silver III] Title: N과 M (4), Time: 164 ms, Memory: 16516 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -3,36 +3,37 @@ import java.io.*;
 
 public class Main {
     static int N, M;
-    static int[] A;
-    static boolean[] visited;
+    static int[] A, result;
     static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         
-        A = new int[M];
-        visited = new boolean[N];
+        A = new int[N];
+        for(int i = 0 ; i < N ; i++) {
+            A[i] = i + 1;
+        }
         
-        dfs(1, 0);
+        result = new int[M];
+        
+        back(0, 0);
         System.out.println(sb);
     }
-
-    private static void dfs(int at, int depth) {
+    
+    private static void back(int start, int depth) {
         if(depth == M) {
-            for(int val : A) {
-                sb.append(val + " ");
-            }
+            for(int i : result) sb.append(i).append(" ");
             sb.append("\n");
             return;
         }
         
-        for(int i = at ; i <= N ; i++) {
-            A[depth] = i;
-            dfs(i, depth + 1);
+        for(int i = start ; i < N ; i++) {
+            result[depth] = A[i];
+            back(i, depth + 1);
         }
     }
 }
