fac316301c541be24b5470c5e053a7efdb52c902
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-08-29 23:52:17 +0900

    [Silver I] Title: 동물원, Time: 140 ms, Memory: 18488 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static final int MOD = 9901;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        long[][] dp = new long[N + 1][3];
+        Arrays.fill(dp[1], 1);
+        
+        for(int i = 2 ; i <= N ; i++) {
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % MOD;
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % MOD;
+            dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % MOD;
+        }
+        
+        long ans = (dp[N][0] + dp[N][1] + dp[N][2]) % MOD;
+        System.out.println(ans);
+    }
+}
