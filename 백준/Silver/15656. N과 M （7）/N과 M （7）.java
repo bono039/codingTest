303432affc259e18dc69f3e2d676141561abb907
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-08-29 19:51:38 +0900

    [Silver III] Title: N과 M (7), Time: 684 ms, Memory: 123672 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[] A, result;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        A = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0 ; i < N ; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        
+        result = new int[M];
+        
+        back(0);
+        System.out.println(sb);
+    }
+    
+    private static void back(int depth) {
+        if(depth == M) {
+            for(int i : result) sb.append(i).append(" ");
+            sb.append("\n");
+            return;
+        }
+        
+        for(int i = 0 ; i < N ; i++) {
+            result[depth] = A[i];
+            back(depth + 1);
+        }
+    }
+}
