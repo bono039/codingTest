ac083b2103b64706b1e03ad9e19dadfd93c94576
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-09-07 17:15:56 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+    
+        while(n > 0) {
+            if(n % 2 == 1) {
+                ans++;
+            }
+            
+            n /= 2;
+        }
+        
+        return ans;
+    }
+}
