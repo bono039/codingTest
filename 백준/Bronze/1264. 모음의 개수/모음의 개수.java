a7a71f47832b016fc9f12351c80aecf933bb5c8c
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-09-10 00:29:07 +0900

    [Bronze IV] Title: 모음의 개수, Time: 120 ms, Memory: 13988 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -1,31 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        for(;;) {
-            String str = sc.nextLine();
-            int count = 0;
-            if(str.equals("#")) {
-                break;
-            }
-
-            str = str.toLowerCase();
-
-            for(int i=0; i<str.length(); i++) {
-               switch(str.charAt(i)) {
-                     case 'a':
-                     case 'e':
-                     case 'i':
-                     case 'o':   
-                     case 'u':
-                        count++;
-                    default: 
-                        break;
-               }
-            }            
-            System.out.println(count);
-        }
-    }
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringBuilder sb = new StringBuilder();
+	    
+		while(true) {
+		    String str = br.readLine().toLowerCase();
+		    
+		    if(str.equals("#")) break;
+		    
+            int cnt = 0;		    
+		    for(int i = 0 ; i < str.length() ; i++) {
+		        char ch = str.charAt(i);
+		        
+		        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+		            cnt++;
+		    }		    
+		    sb.append(cnt).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 }
