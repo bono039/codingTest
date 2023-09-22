06f843bc054adcd13dc8477ddbfaaf1207c44731
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-09-22 22:23:07 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        
+        for(int i : numbers) {
+            answer += i;
+            
+            if(answer > n) {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
