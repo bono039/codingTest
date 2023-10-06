79764ba59eb9a55aa54ca7105ccd9e90a1cc44e6
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-10-06 20:49:22 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 6.47 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String myString, String pat) {
+        myString = myString.toLowerCase();
+        pat = pat.toLowerCase();
+        
+        return myString.contains(pat) ? 1 : 0;
+    }
+}
