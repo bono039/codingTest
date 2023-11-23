4ff876d52a886ff257d4d5fa66a7ef280fc62e61
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-23 16:29:29 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 23.68 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,41 +2,46 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int answer = 0;
-
-        long sum1 = 0;  // 1번 큐의 합
+        int answer = 0; // 필요한 작업의 최소 횟수
+        
+        long sum1 = 0;
         long sum2 = 0;
         
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
+        Deque<Integer> deque1 = new ArrayDeque<>();
+        Deque<Integer> deque2 = new ArrayDeque<>();
         
-        for(int i = 0 ; i < queue1.length ; i++) {           
+        for(int i = 0 ; i < queue1.length ; i++) {
+            deque1.add(queue1[i]);
+            deque2.add(queue2[i]);
+            
             sum1 += queue1[i];
             sum2 += queue2[i];
-            
-            q1.add(queue1[i]);
-            q2.add(queue2[i]);
         }
-                
+        
+        
         long target = (sum1 + sum2) / 2;
         
         while(sum1 != sum2) {
-            if(answer >= queue1.length * 3)    // 더 순회해도 못 만듦
+            // 큐를 최대 3번까지 순회하며 합 맞출 수 있음
+            if(answer >= queue1.length * 3) {
                 return -1;
-            
-            if(sum1 > sum2) {
-                sum1 -= q1.peek();
-                q2.add(q1.peek());
-                sum2 += q1.poll();
             }
-            else {
-                sum2 -= q2.peek();
-                q1.add(q2.peek());
-                sum1 += q2.poll();
+            
+            if(sum1 < sum2) {
+                deque1.addLast(deque2.peekFirst());
+                sum1 += deque2.peekFirst();
+                
+                sum2 -= deque2.pollFirst();
+            }
+            else if(sum1 > sum2) {
+                deque2.addLast(deque1.peekFirst());
+                sum2 += deque1.peekFirst();
+                
+                sum1 -= deque1.pollFirst();
             }
             
             answer++;
-        }
+        }        
         
         return answer;
     }
