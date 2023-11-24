dae89133a7f828ea79f0d89572b5ce3113164610
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-25 00:35:50 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.23 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    static Map<String, String> map = new HashMap<>();
+    static List<Integer> list = new ArrayList<>();
+    
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        
+        // 문자와 그에 따른 기간 넣기
+        for(String term : terms) {
+            String[] str = term.split(" ");
+            map.put(str[0], str[1]);
+        }
+        
+        int YY = Integer.parseInt(today.split("\\.")[0]);
+        int MM = Integer.parseInt(today.split("\\.")[1]);
+        int DD = Integer.parseInt(today.split("\\.")[2]);
+        
+        // 개인정보 수집일자
+        for(int i = 0 ; i < privacies.length ; i++) {
+            String[] privacy = privacies[i].split(" ");
+            String[] numbers = privacy[0].split("\\.");
+
+            int type = Integer.parseInt(map.get(privacy[1])) * 28;
+            
+            int num = (YY - Integer.parseInt(numbers[0])) * 28 * 12
+                    + (MM - Integer.parseInt(numbers[1])) * 28
+                    + (DD - Integer.parseInt(numbers[2]));
+            
+            if(num >= type) {
+                list.add(i + 1);
+            }                         
+        }      
+        
+        // 출력하기
+        Collections.sort(list);
+        int[] answer = new int[list.size()];  // 파기해야 할 개인정보 번호
+        for(int i = 0 ; i < answer.length ; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+    
+    private static boolean getDiff(String target, String curr, int num) {
+        String[] targetStr = target.split(".");
+        String[] currStr = curr.split(".");
+        
+        int diff = 0;
+        diff += (Integer.parseInt(targetStr[0]) - Integer.parseInt(currStr[0])) + (Integer.parseInt(targetStr[1]) - Integer.parseInt(currStr[1])) + (Integer.parseInt(targetStr[2]) - Integer.parseInt(currStr[2]));
+        
+        if(diff <= 28 * num) {
+            return true;
+        }
+        return false;
+    }
+}
