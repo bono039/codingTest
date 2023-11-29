0438dce69dcd904261b97f3ee0a3a7e1b5dffa8c
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-11-29 15:44:47 +0900

    [Silver III] Title: 대표 자연수, Time: 220 ms, Memory: 16984 KB -BaekjoonHub

--- 백준/Silver/2548. 대표 자연수/대표 자연수.java
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.java
@@ -8,32 +8,14 @@ public class Main {
         int N = Integer.parseInt((br.readLine()));
 
         int[] arr = new int[N];
-        int sum = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0 ; i < N ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            sum += arr[i];
         }
         Arrays.sort(arr);
         
-        int tmp1 = (arr.length % 2 == 0) ? arr[arr.length / 2 - 1] : arr[arr.length / 2];  // -1은 나중에 생각해
-        int diffSum = 0;
-        for(int i = 0 ; i < N ; i++) {
-            diffSum += Math.abs(arr[i] - tmp1);
-        }
-
-        int avg = sum / arr.length;
-        int tmp2 = 0;
-        for(int i : arr) {
-            tmp2 += Math.abs(avg - i);
-        }
-
-        if(tmp1 >= tmp2) {
-            System.out.println(avg);
-        }
-        else {
-            System.out.println(tmp1);
-        }
+        int mid = (arr.length % 2 == 0) ? arr[arr.length / 2 - 1] : arr[arr.length / 2];  // -1은 나중에 생각해
+        System.out.println(mid);
     }
 }
