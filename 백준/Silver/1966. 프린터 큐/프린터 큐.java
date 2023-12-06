737ab20a16a4e35b156458f69c76f5590bd9d7d7
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-12-06 14:48:03 +0900

    [Silver III] Title: 프린터 큐, Time: 152 ms, Memory: 15316 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -3,51 +3,61 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    StringBuilder sb = new StringBuilder();
-	    
-	    int T = Integer.parseInt(br.readLine());
-	    
-	    while(T --> 0) {
-	        StringTokenizer st = new StringTokenizer(br.readLine());
-	       
-	        int N = Integer.parseInt(st.nextToken());
-	        int M = Integer.parseInt(st.nextToken());
-	        
-	        LinkedList<int[]> queue = new LinkedList<>();
-	        
-	        st = new StringTokenizer(br.readLine());
-	        for(int i = 0 ; i < N ; i++) {
-	            queue.add(new int[] {i, Integer.parseInt(st.nextToken())});
-	        } 
-	        
-	        int cnt = 0;
-	        
-	        while(!queue.isEmpty()) {
-                boolean isMax = true;
-                int now[] = queue.remove();
-                
-                for(int i = 0 ; i < queue.size() ; i++) {
-                    if(now[1] < queue.get(i)[1]) {
-                        queue.offer(now);
-                        
-                        for(int j = 0 ; j < i ; j++) {
-                            queue.offer(queue.poll());
-                        }
-                        
-                        isMax = false;
-                        break;
-                    }
-                }
-	               
-                if(isMax) {
-                    cnt++;
-                    if(now[0] == M) break;
-                }
-            }
-	        sb.append(cnt).append("\n");
-	    }
-	    
-		System.out.print(sb);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		while(T --> 0) {
+		    st = new StringTokenizer(br.readLine(), " ");
+		    int N = Integer.parseInt(st.nextToken());
+		    int M = Integer.parseInt(st.nextToken());
+		    
+		    LinkedList<Node> q = new LinkedList<>();
+		    
+		    st = new StringTokenizer(br.readLine(), " ");
+		    for(int i = 0 ; i < N ; i++) {
+		        q.add(new Node(i, Integer.parseInt(st.nextToken())));
+		    }
+		    
+		    int cnt = 0;
+		    
+		    while(!q.isEmpty()) {
+		        boolean isMax = true;
+		        Node now = q.poll();
+		        
+		        for(int i = 0 ; i < q.size() ; i++) {
+		            if(now.val < q.get(i).val) {
+		                q.add(now);
+		                
+		                for(int j = 0 ; j < i ; j++) {
+		                    q.add(q.poll());
+		                }
+		                
+		                isMax = false;
+		                break;
+		            }
+		        }
+		        
+		        
+		        if(isMax) {
+		            cnt++;
+		            if(now.idx == M) break;
+		        }
+		    }
+		    sb.append(cnt).append("\n");
+		}
+		
+		System.out.println(sb);
 	}
+}
+
+class Node  {
+    int idx, val;
+    
+    public Node(int idx, int val) {
+        this.idx = idx;
+        this.val = val;
+    }
 }
