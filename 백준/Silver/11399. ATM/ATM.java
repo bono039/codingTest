f25116b4a202aef69e5af17e8e8cf0d0d7ea8227
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-12-21 21:51:36 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,27 +1,28 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int N;
+    static int[] arr;
+    
 	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N + 1];
-        
-        String[] s = br.readLine().split(" ");
-        for(int i = 0 ; i < N ; i++) {
-            A[i] = Integer.parseInt(s[i]);
-        }
-        Arrays.sort(A);
-        
-        int[] dp = new int[N + 1];
-        dp[1] = A[1];
-        
-        int total = dp[1];
-        for(int i = 2 ;  i <= N ; i++) {
-            dp[i] = dp[i-1] + A[i];
-            total += dp[i];
-        }
-        System.out.println(total);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0 ; i < N ; i++) {
+		    arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		int sum = arr[0];
+		for(int i = 1 ; i < N ; i++) {
+		    arr[i] += arr[i - 1];
+		    sum += arr[i];
+		}
+		
+		System.out.println(sum);
 	}
 }
