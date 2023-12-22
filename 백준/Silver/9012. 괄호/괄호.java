820d4347b6e92b9d85d9742356da7fc7efca4ce2
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-12-22 14:11:51 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -6,38 +6,29 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		int T = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		
-		while(T --> 0) {
-		    String str = br.readLine();
+		while(N --> 0) {
+		    char[] ch = br.readLine().toCharArray();
+		    Stack<Character> stack = new Stack<>();
 		    
-		    sb.append(solve(str) ? "YES" : "NO");
-		    sb.append("\n");
+		    for(int i = 0 ; i < ch.length ; i++) {
+		        if(ch[i] == '(') {
+		            stack.push('(');
+		        }
+		        else {
+		            if(!stack.isEmpty() && stack.peek() == '(') {
+		                stack.pop();
+		            }
+		            else {
+		                stack.push(')');
+		            }
+		        }
+		    }
+		    
+		    sb.append(stack.isEmpty() ? "YES" : "NO").append("\n");
 		}
 		
 		System.out.println(sb);
-	}
-	
-	private static boolean solve(String str) {
-    	Stack<Character> stack = new Stack<>();
-	    char[] ch = str.toCharArray();
-	    
-	    for(int i = 0 ; i < ch.length ; i++) {
-	        if(ch[i] == '(') {
-	            stack.push('(');
-	        }
-	        else {
-	            if(!stack.isEmpty() && stack.peek() == '(') {
-	                stack.pop();
-	            }
-	            else {
-	                stack.push(ch[i]);
-	            }
-	        }
-	    }
-	    
-	    if(stack.isEmpty())
-	        return true;
-	    return false;
 	}
 }
