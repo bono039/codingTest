48902d8c29a6c33c5e71f50b11b043d96035aa02
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-12-23 19:00:38 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 832 ms, Memory: 33808 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -2,74 +2,69 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dx = {0, 1, 0, -1};
+    static int[] dx = {0, 1, 0, -1};   // 반시계 방향
     static int[] dy = {1, 0, -1, 0};
     
     static int N, M, R;
-    static int min;
-    static int[][] map;
+    static int[][] board;
     
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		for(int i = 0 ; i < N ; i++) {
-		    st = new StringTokenizer(br.readLine(), " ");
-		    for(int j = 0 ; j < M ; j++) {
-		        map[i][j] = Integer.parseInt(st.nextToken());
-		    }
-		}
-		
-		min = Math.min(N, M);
-		
-		while(R --> 0) {
-		    rotate();
-		}
-
-		print();
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    
+	    N = Integer.parseInt(st.nextToken());
+	    M = Integer.parseInt(st.nextToken());
+	    R = Integer.parseInt(st.nextToken());
+	    
+	    board = new int[N][M];
+	    for(int i = 0 ; i < N ; i++) {
+	        st = new StringTokenizer(br.readLine(), " ");
+	        for(int j = 0 ; j < M ; j++) {
+	            board[i][j] = Integer.parseInt(st.nextToken());
+	        }
+	    }
+	    
+	    while(R --> 0) {
+	        rotate();
+	    }
+	    
+	    StringBuilder sb = new StringBuilder();
+	    for(int[] i : board) {
+	        for(int j : i) {
+	            sb.append(j + " ");
+	        }
+	        sb.append("\n");
+	    }
+	    System.out.println(sb);
 	}
 	
 	private static void rotate() {
-	    for(int i = 0 ; i < min / 2 ; i++) {
+	    for(int i = 0 ; i < Math.min(N, M) / 2 ; i++) {
 	        int x = i;
 	        int y = i;
 	        
-	        int lastVal = map[x][y];
+	        int lastVal = board[x][y];
 	        
 	        int idx = 0;
-	        
 	        while(idx < 4) {
 	            int nx = x + dx[idx];
-	            int ny = y + dy[idx];
-	            
-	            if(i <= nx && nx < N - i && i <= ny && ny < M - i) {
-	                map[x][y] = map[nx][ny];
-    	            x = nx;
-    	            y = ny; 
-	            }
-	            else {
-	                idx++;
-	            }
+                int ny = y + dy[idx];
+                
+                if(!inRange(i, nx, ny)) {
+                    idx++;
+                }
+                else {
+                    board[x][y] = board[nx][ny];
+                    x = nx;
+                    y = ny;
+                }
 	        }
 	        
-	        map[i + 1][i] = lastVal;
+	        board[x + 1][y] = lastVal;
 	    }
 	}
 	
-	private static void print() {
-	    StringBuilder sb = new StringBuilder();
-	    
-		for(int[] i : map) {
-		    for(int j : i) {
-		        sb.append(j + " ");
-		    }
-		    sb.append("\n");
-		}
-		System.out.println(sb);	    
+	private static boolean inRange(int i, int x, int y) {
+	    return (i <= x && x < N - i && i <= y && y < M - i);
 	}
 }
