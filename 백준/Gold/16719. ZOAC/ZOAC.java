2006684d32c4639df4646e9858b9247a0d2957b2
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-12-23 21:26:28 +0900

    [Gold V] Title: ZOAC, Time: 124 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Gold/16719. ZOAC/ZOAC.java
+++ 백준/Gold/16719. ZOAC/ZOAC.java
@@ -2,18 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static String str;
+    static String word;
     static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
     
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		str = br.readLine();
-		visited = new boolean[str.length()];
-		
-		zoac(0, str.length() - 1);
-		System.out.println(sb);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	    word = br.readLine();
+	    visited = new boolean[word.length()];
+	    
+	    zoac(0, word.length() - 1);
+	    System.out.println(sb);
 	}
 	
 	private static void zoac(int left, int right) {
@@ -21,20 +21,21 @@ public class Main {
 	    
 	    int idx = left;
 	    for(int i = left ; i <= right ; i++) {
-	        if(str.charAt(idx) > str.charAt(i)) {
+	        if(word.charAt(idx) > word.charAt(i)) {
 	            idx = i;
 	        }
 	    }
 	    visited[idx] = true;
 	    
-	    for(int i = 0 ; i < str.length() ; i++) {
+	    for(int i = 0 ; i < word.length() ; i++) {
 	        if(visited[i]) {
-	            sb.append(str.charAt(i));
+	            sb.append(word.charAt(i));
 	        }
 	    }
 	    sb.append("\n");
 	    
 	    zoac(idx + 1, right);
 	    zoac(left, idx - 1);
+
 	}
 }
