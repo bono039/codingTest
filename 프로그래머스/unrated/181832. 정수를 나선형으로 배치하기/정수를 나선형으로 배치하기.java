f6a5d2b33d3fba01f4e19d75ba31da7d6241969c
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2023-12-27 12:07:35 +0900

    [level unrated] Title: 정수를 나선형으로 배치하기, Time: 0.12 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,56 +1,38 @@
 import java.util.*;
 
-class Solution {    
-    static int val = 1;
-    static int row, col;    // 현재 행, 열 위치
-    static int dir = 0;     // 방향 (0 : 오른쪽 / 1 : 아래 / 2 : 왼쪽 / 3 : 위쪽)
+class Solution {
+    static int n;
+    
+    static int[] dx = {0,1,0,-1};   // 동남서북
+    static int[] dy = {1,0,-1,0};
     
     public int[][] solution(int n) {
+        this.n = n;
         int[][] answer = new int[n][n];
         
-        while(val <= n * n) {   // 모두 채워질 때까지 반복
-            answer[row][col] = val++;
-            
-            // 다음 이동할 위치 계산
-            if(dir == 0) {  // 오른쪽 방향으로 이동
-                if(col == n - 1 || answer[row][col + 1] != 0) {
-                    dir = 1;
-                    row++;
-                }
-                else {
-                    col++;
-                }
-            }
-            else if(dir == 1) { // 아래쪽 방향으로 이동
-                if(row == n - 1 || answer[row + 1][col] != 0) {
-                    dir = 2;
-                    col--;
-                }
-                else {
-                    row++;
-                }
-            }
-            else if(dir == 2) { // 왼쪽 방향으로 이동
-                if(col == 0 || answer[row][col - 1] != 0) {
-                    dir = 3;
-                    row--;
-                }
-                else {
-                    col--;
-                }
-            }
-            else if(dir == 3) { // 위쪽 방향으로 이동
-                if(row == 0 || answer[row - 1][col] != 0) {
-                    dir = 0;
-                    col++;
-                }
-                else {
-                    row--;
-                }
-            }
-        }
+        answer[0][0] = 1;
+        int dir = 0;
         
+        int x = 0;
+        int y = 0;
+        
+        for(int i = 2 ; i <= n * n ; i++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            
+            if(!inRange(nx, ny) || answer[nx][ny] != 0) {
+                dir = (dir + 1) % 4;
+            }
+            
+            x += dx[dir];
+            y += dy[dir];
+            answer[x][y] = i;            
+        }        
         
         return answer;
+    }
+    
+    private static boolean inRange(int r, int c) {
+        return (0 <= r && r < n && 0 <= c && c < n);
     }
 }
