7d7a2926eec5895b6b7a6c7f617c89727ec6813a
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-01-04 15:18:02 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10008 ms, Memory: 304964 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -3,63 +3,66 @@ import java.io.*;
 
 public class Main {
     static int N, M;
-    static ArrayList<Integer>[] A;
+    static List<Integer>[] A;
     static boolean[] visited;
-    static int[] answer;
-	static int max = 0;
+    
+    static int maxDepth = -1;
+    static int[] cntArr;
     
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		A = new ArrayList[N + 1];
-		for(int i = 1 ; i <= N ; i++) {
-		    A[i] = new ArrayList<>();
-		}
-		
-		visited = new boolean[N + 1];
-		answer = new int[N + 1];
-		
-		while(M --> 0) {
-		    st = new StringTokenizer(br.readLine(), " ");
-		    
-		    int s = Integer.parseInt(st.nextToken());
-		    int e = Integer.parseInt(st.nextToken());
-		    
-		    A[s].add(e);
-		}
-		
-		for(int i = 1 ; i <= N ; i++) {
-		    visited = new boolean[N + 1];
-		    bfs(i);
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 1 ; i <= N ; i++) {
-    	    if(answer[i] == max) {
-    	        sb.append(i).append(" ");
-    	    }
-		}
-		System.out.println(sb);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    
+	    N = Integer.parseInt(st.nextToken());
+	    M = Integer.parseInt(st.nextToken());
+	    
+	    A = new ArrayList[N + 1];
+	    for(int i = 1 ; i <= N ; i++)   A[i] = new ArrayList<>();
+	    
+	    while(M --> 0) {
+	        st = new StringTokenizer(br.readLine(), " ");
+	        int a = Integer.parseInt(st.nextToken());
+	        int b = Integer.parseInt(st.nextToken());
+	        
+	        A[a].add(b);
+	    }
+	    
+	    cntArr = new int[N + 1];
+	    
+	    for(int i = 1 ; i <= N ; i++) {
+	        visited = new boolean[N + 1];
+	        
+	        bfs(i);
+	    }
+	    
+	    StringBuilder sb = new StringBuilder();
+	    for(int i = 1 ; i <= N ; i++) {
+	        if(cntArr[i] > maxDepth) {
+	            maxDepth = cntArr[i];
+	        }
+	    }
+	    
+	    for(int i = 1 ; i <= N ; i++) {
+	        if(maxDepth == cntArr[i]) {
+	            System.out.print(i + " ");
+	        }
+	    }
 	}
 	
-	static void bfs(int num) {
-	    Queue<Integer> queue = new LinkedList<>();
-	    queue.add(num);
+	private static void bfs(int num) {
+	    Queue<Integer> q = new ArrayDeque<>();
+	    q.add(num);
+	    
 	    visited[num] = true;
 	    
-	    while(!queue.isEmpty()) {
-	        int now = queue.poll();
+	    while(!q.isEmpty()) {
+	        int now = q.poll();
 	        
 	        for(int next : A[now]) {
 	            if(!visited[next]) {
+	                q.add(next);
 	                visited[next] = true;
-	                answer[next]++;
-	                queue.add(next);
-	                max = Math.max(max, answer[next]);
+	                cntArr[next]++;
 	            }
 	        }
 	    }
