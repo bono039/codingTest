43384f8f4b14eaf17243669a890b2546cd05f988
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-01-06 00:58:08 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,56 +2,61 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dx = {0, 0, -1, 1};    // 좌우
-    static int[] dy = {1, -1, 0, 0};    // 상하
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     
+    static int N, M;
+    static int[][] map;
     static boolean[][] visited;
-    static int[][] A;
     
-    static int N, M;    
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		visited = new boolean[N][M];
-		
-		A = new int[N][M];  // 101?
-		
-		// 입력 받기
-		for(int i = 0 ; i < N ; i++) {
-		    String str = br.readLine();
-		    for(int j = 0 ; j < M ; j++) {
-		        A[i][j] = str.charAt(j) - '0';
-		    }
-		}
-		
-        BFS(0, 0);
-		System.out.println(A[N - 1][M - 1]);
-	}
-	
-	static void BFS(int x, int y) {
-	    visited[x][y] = true;
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 	    
-	    Queue<int[]> queue = new LinkedList<>();
-	    queue.add(new int[] {x, y});
+	    N = Integer.parseInt(st.nextToken());
+	    M = Integer.parseInt(st.nextToken());
 	    
-	    while(!queue.isEmpty()) {
-	        int[] now = queue.poll();
-	        
-	        // 상하좌우 이동
-	        for(int d = 0 ; d < 4 ; d++) {
-	            int newX = now[0] + dx[d];
-	            int newY = now[1] + dy[d];
-	            
-	            if(0 <= newX && newX < N && 0 <= newY && newY < M && !visited[newX][newY] && A[newX][newY]!= 0) {
-	                visited[newX][newY] = true;
-	                A[newX][newY] = A[now[0]][now[1]] + 1;  //  깊이 업데이트
-	                queue.add(new int[]{newX, newY});
-	            }       
+	    map = new int[N][M];
+	    
+	    for(int i = 0 ; i < N ; i++) {
+	        String s = br.readLine();
+	        for(int j = 0 ; j < M ; j++) {
+	            map[i][j] = s.charAt(j) - '0';
 	        }
 	    }
+	    
+	    visited = new boolean[N][M];
+	    bfs(0, 0);
+	    
+	    System.out.println(map[N - 1][M - 1]);
+	}
+	
+	private static void bfs(int x, int y) {
+	    Queue<int[]> q = new ArrayDeque<>();
+	    q.add(new int[] {x, y});
+	    
+	    visited[x][y] = true;
+	    
+	    while(!q.isEmpty()) {
+	        int[] now = q.poll();
+	        
+	        int nowX = now[0];
+	        int nowY = now[1];
+	        
+	        for(int d = 0 ; d < 4 ; d++) {
+	            int nx = nowX + dx[d];
+	            int ny = nowY + dy[d];
+	            
+	            if(!inRange(nx, ny) || map[nx][ny] == 0 || visited[nx][ny]) continue;
+
+	            visited[nx][ny] = true;
+	            map[nx][ny] = map[nowX][nowY] + 1;
+	            q.add(new int[] {nx, ny});
+	        }
+	    }
+	}
+	
+	private static boolean inRange(int x, int y) {
+	    return (0 <= x && x < N && 0 <= y && y < M);
 	}
 }
