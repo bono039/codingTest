bdec27a1f8f7e3d55e5a1506219c73aeba9c3c12
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-01-12 11:47:06 +0900

    [Silver II] Title: 배열 돌리기, Time: 1048 ms, Memory: 162108 KB -BaekjoonHub

---
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {    
+    static int T, n, d;
+    static int[][] grid, copy;
+    static StringBuilder sb = new StringBuilder();
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    
+	    T = Integer.parseInt(br.readLine());
+	    
+	    while(T --> 0) {
+	        st = new StringTokenizer(br.readLine(), " ");
+	        n = Integer.parseInt(st.nextToken());
+	        d = Integer.parseInt(st.nextToken());
+	        
+	        grid = new int[n][n];
+	        copy = new int[n][n];
+	        for(int i = 0 ; i < n ; i++) {
+	            st = new StringTokenizer(br.readLine(), " ");
+	            for(int j = 0 ; j < n ; j++) {
+	                grid[i][j] = Integer.parseInt(st.nextToken());
+	                copy[i][j] = grid[i][j];
+	            }
+	        }
+	        
+	        if(d < 0) d += 360;
+	        if(d >= 360) d -= 360;
+	        
+	        int cnt = d/45;
+	        while(cnt --> 0) {
+	            rotate();
+	        }
+	        
+            for(int[] i : grid) {
+                for(int j : i) {
+                    sb.append(j + " ");
+                }
+                sb.append("\n");
+            }
+	    }
+	    
+	    System.out.println(sb);
+	}
+	
+	private static void rotate() {
+	    for(int i = 0 ; i < n ; i++) {
+	        copy[i][n / 2] = grid[i][i];
+	        copy[i][i] = grid[n / 2][i];
+	        copy[n / 2][i] = grid[n - i - 1][i];
+	        copy[n - i - 1][i] = grid[n - i - 1][n / 2];
+	    }
+	    
+	    for(int i = 0 ; i < n ; i++) {
+	        for(int j = 0 ; j < n ; j++) {
+	            grid[i][j] = copy[i][j];
+	        }
+	    }
+	}
+}
