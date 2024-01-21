37dfefc4b59d7baa8e2eded8ab3cbe36f468b438
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-01-21 16:14:28 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 66.83 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,25 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int answer = 0; // 필요한 작업의 최소 횟수
+        int answer = 0;
         
         long sum1 = 0;
         long sum2 = 0;
         
-        Deque<Integer> deque1 = new ArrayDeque<>();
-        Deque<Integer> deque2 = new ArrayDeque<>();
+        Deque<Integer> dq1 = new ArrayDeque<>();
+        Deque<Integer> dq2 = new ArrayDeque<>();
         
         for(int i = 0 ; i < queue1.length ; i++) {
-            deque1.add(queue1[i]);
-            deque2.add(queue2[i]);
+            dq1.add(queue1[i]);
+            dq2.add(queue2[i]);
             
             sum1 += queue1[i];
             sum2 += queue2[i];
         }
-        
-        
-        long target = (sum1 + sum2) / 2;
-        
+               
         while(sum1 != sum2) {
             // 큐를 최대 3번까지 순회하며 합 맞출 수 있음
             if(answer >= queue1.length * 3) {
@@ -28,21 +25,21 @@ class Solution {
             }
             
             if(sum1 < sum2) {
-                deque1.addLast(deque2.peekFirst());
-                sum1 += deque2.peekFirst();
-                
-                sum2 -= deque2.pollFirst();
+                int num = dq2.pollFirst();
+                dq1.addLast(num);
+                sum1 += num;
+                sum2 -= num;
             }
             else if(sum1 > sum2) {
-                deque2.addLast(deque1.peekFirst());
-                sum2 += deque1.peekFirst();
-                
-                sum1 -= deque1.pollFirst();
+                int num = dq1.pollFirst();
+                dq2.addLast(num);
+                sum2 += num;
+                sum1 -= num;
             }
             
             answer++;
-        }        
-        
+        }
+                
         return answer;
     }
 }
