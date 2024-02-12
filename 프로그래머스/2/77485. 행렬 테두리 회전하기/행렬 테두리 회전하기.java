a4be615d43bf5bd6cb378df5e100acca21c6c6ba
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-12 12:09:42 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 15.00 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,67 +1,55 @@
 import java.util.*;
-// 테두리에 있는 숫자, 시계 방향 회전
+
 class Solution {
-    static int[] dx = {1,0,-1,0};   // 동남서북
-    static int[] dy = {0,1,0,-1};
-    
-    static int rows, columns;
-    static int[][] queries, arr;
-    static int min;
+    // 회전 방향 : 시계 방향 ⇒ 끌어올 방향 : 반시계 방향 (남동북서)
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
     
     public int[] solution(int rows, int columns, int[][] queries) {
-        this.rows = rows;
-        this.columns = columns;
-        this.queries = queries;
-        
         int[] answer = new int[queries.length];
         
-        // 배열 초기화
-        arr = new int[rows][columns];
+        int[][] grid = new int[rows][columns];
+        
         int val = 1;
         for(int i = 0 ; i < rows ; i++) {
-            for(int j = 0 ; j < columns ; j++) {
-                arr[i][j] = val++;
-            }
+            for(int j = 0 ; j < columns ; j++)
+                grid[i][j] = val++;
         }
         
-        for(int i = 0 ; i < queries.length ; i++) {
-            min = Integer.MAX_VALUE;
-            answer[i] = simulate(queries[i]);
-        }      
+        int idx = 0;
+                
+        for(int[] q : queries) {
+            int x1 = q[0] - 1;
+            int y1 = q[1] - 1;
+            int x2 = q[2] - 1;
+            int y2 = q[3] - 1;
+            
+            int x = x1;
+            int y = y1;
+            int dir = 0;
+            
+            int firstNum = grid[x][y];
+            int min = firstNum;
+            
+            while(dir < 4) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                
+                if(nx < x1 || nx > x2 || ny < y1 || ny > y2) {
+                    dir++;
+                }
+                else {
+                    grid[x][y] = grid[nx][ny];
+                    min = Math.min(min, grid[x][y]);
+                    x = nx;
+                    y = ny;
+                }
+            }
+            
+            grid[x][y + 1] = firstNum;
+            answer[idx++] = min;
+        }
         
         return answer;
-    }
-    
-    private static int simulate(int[] query) {
-        int x1 = query[0] -1;
-        int y1 = query[1] -1;
-        int x2 = query[2] -1;
-        int y2 = query[3] -1;
-        
-        int tmp = arr[x1][y1];
-        int dir = 0;    // 시작 방향 : 동쪽
-        
-        int x = x1;
-        int y = y1;
-        
-        while(dir < 4) {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-            
-            if(nx < x1 || ny < y1 || nx > x2 || ny > y2) {  // 지정 범위 벗어나면, 회전 방향 번경
-                dir++;
-            }
-            else {
-                arr[x][y] = arr[nx][ny];
-                min = Math.min(min, arr[x][y]);
-                x = nx;
-                y = ny;
-            }
-        }
-        
-        arr[x][y + 1] = tmp;
-        min = Math.min(min, arr[x][y + 1]);
-        
-        return min;       
     }
 }
