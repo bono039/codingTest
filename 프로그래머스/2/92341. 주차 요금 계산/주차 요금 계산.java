fc20467c845c95925b509933a6306b35544a845e
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-17 21:03:54 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.49 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,55 +1,105 @@
 import java.util.*;
 
 class Solution {
+    //                      주차 요금(기본 시간 / 기본 요금 / 단위 시간 / 단위 요금)     자동차 입/출차 내역 (시각 차량번호 내역)
     public int[] solution(int[] fees, String[] records) {
-        
-        Map<String, String> map = new HashMap<>();  // 차 번호, 시간
-        Map<String, Integer> feeMap = new TreeMap<>();  // 차 번호, 요금 (차량번호 기준 오름차순 정렬)
-        
-        for(int i = 0 ; i < records.length ; i++) {
-            feeMap.put(records[i].split(" ")[1], 0);
-        }
-        
-        // IN-OUT 기록 둘 다 있는 경우
-        for(int i = 0 ; i < records.length ; i++) {
-            String[] infos = records[i].split(" ");
+        int basicTime = fees[0];
+        int basicFee = fees[1];
+        int perTime = fees[2];
+        int perFee = fees[3];
             
-            if(map.containsKey(infos[1])) {
-                String[] inTime = map.remove(infos[1]).split(":");
-                String[] outTime = infos[0].split(":");
-                
-                int HH = Integer.parseInt(outTime[0]) - Integer.parseInt(inTime[0]);
-                int MM = Integer.parseInt(outTime[1]) - Integer.parseInt(inTime[1]);
-                
-                feeMap.replace(infos[1], feeMap.get(infos[1]) + 60 * HH + MM);
+        Map<String, String> map = new HashMap<>();  // 차량 번호, 시각
+        Map<String, Integer> map2 = new HashMap<>();    // 차량 번호, 누적 주차 시간
+        
+        for(int i = 0 ; i < records.length ; i++) {
+            String[] info = records[i].split(" ");
+            
+            String time = info[0];            
+            String carNum = info[1];
+            String cmd = info[2];
+            
+            if(cmd.equals("IN")) {
+                if(!map.containsKey(carNum) && !info[0].equals("23:59")) {
+                    map.put(carNum, time);
+                }  
             }
             else {
-                map.put(infos[1], infos[0]);    // 차 번호, 시간
+                if(map.containsKey(carNum)) {
+                    String[] startInfo = map.get(carNum).split(":");
+                    int startH = Integer.parseInt(startInfo[0]);
+                    int startM = Integer.parseInt(startInfo[1]);
+                    
+                    String[] endInfo = time.split(":");
+                    int endH = Integer.parseInt(endInfo[0]);
+                    int endM = Integer.parseInt(endInfo[1]);
+                    
+                    // 누적 주차 시간 세기
+                    int tmpTime = 0;
+                    
+                    int HH = endH - startH;
+                    int MM = 0;
+                    
+                    if(endM < startM) {
+                        MM = (endM + 60) - startM;
+                        HH--;
+                    }
+                    else {
+                        MM = endM - startM;
+                    }
+                    
+                    tmpTime = 60 * HH + MM;                    
+                    map2.put(carNum, map2.getOrDefault(carNum, 0) + tmpTime);   // 누적 주차 시간 저장
+                    
+                    map.remove(carNum); // 키 삭제
+                }
             }
         }
         
-        // IN 기록만 있는 경우
-        for(String key : map.keySet()) {
-            String[] inTime = map.get(key).split(":");
-            
-            int HH = 23 - Integer.parseInt(inTime[0]);
-            int MM = 59 - Integer.parseInt(inTime[1]);
-            
-            feeMap.replace(key, feeMap.get(key) + 60 * HH + MM);
+        // 남은 주차 중인 차들 처리
+        for(String carNum : map.keySet()) {
+            String[] time = map.get(carNum).split(":");
+
+            int tmpTime = (23 - Integer.parseInt(time[0])) * 60 + (59 - Integer.parseInt(time[1]));
+
+            map2.put(carNum, map2.getOrDefault(carNum, 0) + tmpTime);
         }
         
-        List<Map.Entry<String,Integer>> list = new ArrayList(feeMap.entrySet());    // 차량번호 기준 정렬       
+        // 주차 요금 계산하기
+        List<Node> list = new ArrayList<>();
         
-        int[] answer = new int[list.size()];
+        for(String carNum : map2.keySet()) {
+            int time = map2.get(carNum);
+            int fee = basicFee;
+            
+            if(time > basicTime) {
+                fee += (int)Math.ceil((time - basicTime) / (double)perTime) * perFee;
+            }
+            list.add(new Node(carNum, time, fee));
+        }     
+        
+        Collections.sort(list);
+        
+        int[] answer = new int[list.size()];  // 차별 주차 요금 (차량 번호가 작은 자동차부터)
         for(int i = 0 ; i < answer.length ; i++) {
-            if(list.get(i).getValue() > fees[0]) {
-                answer[i] = fees[1] + (int)Math.ceil((list.get(i).getValue() - fees[0]) / (double)fees[2]) * fees[3];
-            }
-            else {
-                answer[i] = fees[1];
-            }
+            answer[i] = list.get(i).money;
         }
-        
         return answer;
+    }
+}
+
+class Node implements Comparable<Node> {
+    String num;
+    int time, money;
+    
+    public Node(String num, int time, int money) {
+        this.num = num;
+        this.time = time;
+        this.money = money;
+    }
+    
+    // 차량 번호 오름차순 정렬
+    @Override
+    public int compareTo(Node n) {
+        return Integer.parseInt(this.num) - Integer.parseInt(n.num);
     }
 }
