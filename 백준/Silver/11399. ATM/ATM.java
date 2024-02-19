c8c0d6c2e665b2083661bc2f6e240f9c8d96d879
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-19 23:38:51 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -6,23 +6,26 @@ public class Main {
     static int[] arr;
     
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0 ; i < N ; i++) {
-		    arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		
-		int sum = arr[0];
-		for(int i = 1 ; i < N ; i++) {
-		    arr[i] += arr[i - 1];
-		    sum += arr[i];
-		}
-		
-		System.out.println(sum);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    
+	    N = Integer.parseInt(br.readLine());
+	    arr = new int[N];
+	    
+	    String[] str = br.readLine().split(" ");
+	    for(int i = 0 ; i < str.length ; i++) {
+	        arr[i] = Integer.parseInt(str[i]);
+	    }
+	    
+	    Arrays.sort(arr);
+	    
+	    int sum = arr[0];
+	    
+	    for(int i = 1 ; i < N ; i++) {
+	        arr[i] += arr[i-1];
+	        sum += arr[i];
+	    }
+	    
+	    System.out.println(sum);
 	}
 }
