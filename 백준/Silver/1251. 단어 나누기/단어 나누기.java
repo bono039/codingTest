4f3a9f93c05ee32b33c3bbad71e3dcde7b8c0a42
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-20 23:07:59 +0900

    [Silver V] Title: 단어 나누기, Time: 140 ms, Memory: 14936 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.java
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -3,37 +3,26 @@ import java.io.*;
 
 public class Main {
     static String str;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        str = br.readLine();
-
-        List<String> list = new ArrayList<>();
-
-        int len = str.length();
-
-        for(int i = 1 ; i < len ; i++) {
-            for(int j = i + 1 ; j < len ; j++) {
-                String tmp = getSolution(i, j);
-                list.add(tmp);
-            }
-        }
-        Collections.sort(list);
-
-        System.out.println(list.get(0));
-    }
-
-    private static String getSolution(int s, int e) {
-        StringBuilder sb = new StringBuilder();
-
-        StringBuilder sb1 = new StringBuilder(str.substring(0, s));
-        StringBuilder sb2 = new StringBuilder(str.substring(s, e));
-        StringBuilder sb3 = new StringBuilder(str.substring(e));
-
-        sb.append(sb1.reverse().toString());
-        sb.append(sb2.reverse().toString());
-        sb.append(sb3.reverse().toString());
-
-        return sb.toString();
-    }
+    static List<String> list = new ArrayList<>();
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    str = br.readLine();
+	    
+	    for(int i = 1 ; i < str.length() - 1 ; i++) {
+	        for(int j = i + 1 ; j < str.length() ; j++) {
+	            StringBuilder sb = new StringBuilder();
+	            
+	            StringBuilder sb1 = new StringBuilder(str.substring(0, i));
+	            StringBuilder sb2 = new StringBuilder(str.substring(i, j));
+	            StringBuilder sb3 = new StringBuilder(str.substring(j));
+	            
+	            sb.append(sb1.reverse()).append(sb2.reverse()).append(sb3.reverse());
+                list.add(sb.toString());
+	        }
+	    }
+	    
+	    Collections.sort(list);
+	    System.out.println(list.get(0));
+	}
 }
