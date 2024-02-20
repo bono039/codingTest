4518c9a7e013d3a8c85e8100e3c4aaf4e15596dc
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-20 23:50:34 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 240 ms, Memory: 15688 KB -BaekjoonHub

--- 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.java
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.java
@@ -2,51 +2,51 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, C;
+    static int N,C;
     static List<Integer> w = new ArrayList<>();
     static boolean exists = false;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0 ; i < N ; i++) {
-            w.add(Integer.parseInt(st.nextToken()));
-
-            if(w.get(i) == C) {
-                System.out.println(1);
-                return;
-            }
-        }
-        Collections.sort(w);   // 이진 탐색 위해 오름차순 정렬
-
-        int start = 0;
-        int end = N - 1;
-        int weight;
-
-        while(start < end) {
-            weight = w.get(start) + w.get(end);
-
-            if(weight > C) {
-                end--;
-            }
-            else if(weight == C) {
-                System.out.println(1);
-                return;
-            }
-            else {
-                if(start < w.indexOf(C - weight) && w.indexOf(C - weight) < end) {
-                    System.out.println(1);
-                    return;
-                }
-                start++;
-            }
-        }
-
-        System.out.println(0);
-    }
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    
+	    N = Integer.parseInt(st.nextToken());
+	    C = Integer.parseInt(st.nextToken());
+	    
+	    st = new StringTokenizer(br.readLine(), " ");
+	    for(int i = 0 ; i < N ; i++) {
+	        w.add(Integer.parseInt(st.nextToken()));
+	        
+	        if(w.get(i) == C) {
+	            System.out.println(1);
+	            return;
+	        }
+	    }
+	    Collections.sort(w);
+	    
+	    int s = 0;
+	    int e = N - 1;
+	    int weight = 0;
+	    
+	    while(s < e) {
+	        weight = w.get(s) + w.get(e);
+	        
+	        if(weight > C) {
+	            e--;
+	        }
+	        else if(weight == C) {
+	            System.out.println(1);
+	            return;
+	        }
+	        else {
+	            if(s < w.indexOf(C - weight) && w.indexOf(C - weight) < e) {
+	                System.out.println(1);
+	                return;
+	            }
+	            s++;
+	        }
+	    }
+	    
+	    System.out.println(0);
+	}
 }
