9baf8e716e6236dc8155cdcbd9d9b48299f40ce2
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-22 00:06:10 +0900

    [Gold V] Title: 두 용액, Time: 392 ms, Memory: 28852 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -2,46 +2,39 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        long[] A = new long[N];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0 ; i < N ; i++) {
-            A[i] = Long.parseLong(st.nextToken());
-        }
-        
-        Arrays.sort(A);
-        
-        long gap = Integer.MAX_VALUE;
-        long[] ans = new long[2];
-        
-        int s = 0;
-        int e = N - 1;
-        
-        long ans1 = 0;
-        long ans2 = 0;
-        
-        while(s < e) {
-            long sum = A[s] + A[e];
-            long tmp = Math.abs(sum);
-            
-            if(tmp < gap) {
-                gap = tmp;
-                ans1 = A[s];
-                ans2 = A[e];
-            }   
-                
-            if(sum < 0) {
-                s++;
-            }
-            else {
-                e--;
-            }
-        }
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    int N = Integer.parseInt(br.readLine());
+	    long[] A = new long[N];
+	    
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    for(int i = 0 ; i < N ; i++) {
+	        A[i] = Long.parseLong(st.nextToken());
+	    }
+	    Arrays.sort(A);
 
-        System.out.println(ans1 + " " + ans2);
-    }
+	    long gap = Integer.MAX_VALUE;
+	    long ans1 = 0;
+	    long ans2 = 0;
+	    
+	    int s = 0;
+	    int e = N - 1;
+	    
+	    while(s < e) {
+	        long sum = A[s] + A[e];
+	        long absSum = Math.abs(sum);
+	        
+	        if(absSum < gap) {
+	            gap = absSum;
+	            ans1 = A[s];
+	            ans2 = A[e];
+	        }
+	        
+	        if(sum < 0) s++;
+	        else        e--;
+	    }
+	    
+	    System.out.println(ans1 + " " + ans2);
+	}
 }
