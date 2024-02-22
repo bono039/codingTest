b995acda4ee2d6f8b57168746bb401865ac88b99
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-22 23:31:02 +0900

    [Silver IV] Title: 스택, Time: 196 ms, Memory: 19868 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,33 +2,29 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N;
-    static Stack<Integer> stack = new Stack<>();
-    static StringBuilder sb = new StringBuilder();
-    
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-	    N = Integer.parseInt(br.readLine());
-	    for(int i = 0 ; i < N ; i++) {
+	    StringBuilder sb = new StringBuilder();
+	    
+	    int N = Integer.parseInt(br.readLine());
+	    
+	    Stack<Integer> stack = new Stack<>();
+	    
+	    while(N --> 0) {
 	        String[] str = br.readLine().split(" ");
 	        
-	        if(str[0].equals("push")) {
+	        if(str[0].equals("push"))   
 	            stack.push(Integer.parseInt(str[1]));
-	        }
-	        else if(str[0].equals("pop")) {
-                sb.append(stack.isEmpty() ? "-1" : stack.pop()).append("\n");
-	        }
-	        else if(str[0].equals("size")) {
+	        else if(str[0].equals("pop"))
+	            sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+	        else if(str[0].equals("size"))
 	            sb.append(stack.size()).append("\n");
-	        }
-	        else if(str[0].equals("empty")) {
+	        else if(str[0].equals("empty"))
 	            sb.append(stack.isEmpty() ? 1 : 0).append("\n");
-	        }
-	        else if(str[0].equals("top")) {
-	            sb.append(stack.isEmpty() ? "-1" : stack.peek()).append("\n");
-	        }
+	        else if(str[0].equals("top"))
+	            sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
 	    }
-	    System.out.println(sb);
+	    
+	    System.out.println(sb.toString());
 	}
 }
