6720b667be07536e412c651a3d8c7bfb36cfde06
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-24 17:45:25 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 152 ms, Memory: 15888 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -3,37 +3,41 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int N = Integer.parseInt(br.readLine());
-		Deque<Node> q = new ArrayDeque<>();
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 1 ; i <= N ; i++) {
-		    q.add(new Node(i, Integer.parseInt(st.nextToken())));
-		}
-		
-		while(!q.isEmpty()) {
-		    sb.append(q.getFirst().idx + " ");
-		    int next = q.poll().val;
-		    
-		    if(q.isEmpty()) break;
-		    
-		    if(next > 0) {
-		        for(int i = 0 ; i < next - 1 ; i++) {
-		            q.addLast(q.pollFirst());
-		        }
-		    }
-		    else {
-		        for(int i = 0 ; i < Math.abs(next) ; i++) {
-		            q.addFirst(q.pollLast());
-		        }
-		    }
-		}
-        
-        System.out.println(sb);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    
+	    StringBuilder sb = new StringBuilder();
+	    
+	    int N = Integer.parseInt(br.readLine());
+	    
+	    List<Node> list = new ArrayList<>();
+	    
+	    st = new StringTokenizer(br.readLine(), " ");
+	    for(int i = 0 ; i < N ; i++) {
+	        list.add(new Node(i + 1, Integer.parseInt(st.nextToken())));
+	    }
+	    
+	    int pos = 0;
+	    int len = 0;
+	    
+	    while(!list.isEmpty()) {
+            len = list.get(pos).val;
+    	    sb.append(list.get(pos).idx + " ");
+    	    list.remove(pos);
+    	    
+    	    if(list.size() == 0) break;
+    	    
+    	    if(len >= 0) {
+    	        pos = (pos + len - 1) % list.size();
+    	    }
+    	    else {
+    	        pos = (pos + len) % list.size();
+    	        
+    	        if(pos < 0) pos += list.size();
+    	    }
+	    }
+	    
+	    System.out.println(sb.toString());
 	}
 }
 
