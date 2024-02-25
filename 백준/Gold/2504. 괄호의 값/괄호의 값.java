c2f8555d6150e6f8a5e995dbcdb5ae124bbe5cd1
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-26 01:15:36 +0900

    [Gold V] Title: 괄호의 값, Time: 120 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -3,61 +3,61 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		char[] ch = br.readLine().toCharArray();
-		Stack<Character> stack = new Stack<>();
-
-		int result = 0;
-		int num = 1;
-		
-		for(int i = 0 ; i < ch.length ; i++) {
-		    if(ch[i] == '(') {
-		        stack.push('(');
-		        num *= 2;
-		    }
-		    else if(ch[i] == ')') {
-		        if(stack.isEmpty()) {
-		            result = 0;
-		            break;
-		        }
-		        
-		        char beforeC = stack.pop();
-		        if(beforeC != '(') {
-		            result = 0;
-		            break;
-		        }
-		        else {
-		            if(ch[i - 1] == '(') {
-		                result += num;
-		            }
-		            num /= 2;
-		        }
-		    }
-		    else if(ch[i] == '[') {
-		        stack.push('[');
-		        num *= 3;
-		    }
-		    else if(ch[i] == ']') {
-		        if(stack.isEmpty()) {
-		            result = 0;
-		            break;
-		        }
-		        
-		        char beforeC = stack.pop();
-		        if(beforeC != '[') {
-		            result = 0;
-		            break;
-		        }
-		        else {
-		            if(ch[i - 1] == '[') {
-		                result += num;
-		            }
-		            num /= 3;
-		        }
-		    }
-		}
-		
-		System.out.println(!stack.isEmpty() ? 0 : result);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    char[] ch = br.readLine().toCharArray();
+	    Stack<Character> stack = new Stack<>();
+	    
+	    int result = 0;
+	    int cnt = 1;
+	    
+	    for(int i = 0 ; i < ch.length ; i++) {
+	        if(ch[i] == '(') {
+	            stack.push('(');
+	            cnt *= 2;
+	        }
+	        else if(ch[i] == ')') {
+                if(stack.isEmpty()) {
+                    result = 0;
+                    break;
+                }
+                
+                char beforeChar = stack.pop();
+                if(beforeChar != '(') {
+                    result = 0;
+                    break;
+                }
+                else {
+                    if(ch[i - 1] == '(') {
+                        result += cnt;
+                    }
+                    cnt /= 2;
+                }	            
+	        }
+	        else if(ch[i] == '[') {
+	            stack.push('[');
+	            cnt *= 3;
+	        }
+	        else if(ch[i] == ']') {
+                if(stack.isEmpty()) {
+                    result = 0;
+                    break;
+                }
+                
+                char beforeChar = stack.pop();
+                if(beforeChar != '[') {
+                    result = 0;
+                    break;
+                }
+                else {
+                    if(ch[i - 1] == '[') {
+                        result += cnt;
+                    }
+                    cnt /= 3;
+                }   
+	        }
+	    }
+	    
+	    System.out.println(stack.isEmpty() ? result : 0);
 	}
 }
