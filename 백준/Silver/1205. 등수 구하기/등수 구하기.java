073d1179e0a3b64d0580800c426d4a56ff6c06be
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-28 01:20:14 +0900

    [Silver IV] Title: 등수 구하기, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -2,40 +2,47 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        int N = Integer.parseInt(st.nextToken());
-        int newScore = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
-        
+    static int N, tsScore, P;
+    static int[] A;
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    
+	    N = Integer.parseInt(st.nextToken());
+	    tsScore = Integer.parseInt(st.nextToken());
+	    P = Integer.parseInt(st.nextToken());
+
         if(N == 0) {
             System.out.println(1);
             return;
         }
-        
-        int[] A = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0 ; i < N ; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-        }
 
-        if(N == P && newScore <= A[A.length - 1]) {
-            System.out.println(-1);
-        }
-        else {
-            int rank = 1;
-            for(int i = 0 ; i < A.length ; i++) {
-                if(newScore < A[i]) {
-                    rank++;
-                }
-                else {
-                    break;
-                }
-            }
-            
-            System.out.println(rank);
-        }  
-    }
+        A = new int[N];
+	    
+	    st = new StringTokenizer(br.readLine(), " ");
+	    for(int i = 0 ; i < N ; i++) {
+	        A[i] = Integer.parseInt(st.nextToken());
+	    }
+	    
+	    int answer = 0;
+	    
+	    if(N == P && tsScore <= A[A.length - 1]) {
+            answer = -1;
+	    }
+	    else {
+	        int rank = 1;
+	        
+	        for(int i = 0 ; i < N ; i++) {
+	            if(tsScore < A[i])
+	                rank++;
+	            else
+	                break;
+	        }
+	        
+	        answer = rank;
+	    }
+	    
+	    System.out.println(answer);
+	}
 }
