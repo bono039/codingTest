2edd0de413861088e7217bd3064c94ae3348ec4b
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-02-28 17:32:01 +0900

    [Gold IV] Title: 게리맨더링, Time: 152 ms, Memory: 15324 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -4,115 +4,99 @@ import java.io.*;
 public class Main {
     static int N;
     static int[] arr;
-    
     static List<Integer>[] A;
-    static boolean[] visited;
+    static boolean[] selected;
     
-    static int min = Integer.MAX_VALUE;
-    static boolean canDivide = false;
+    static int diff = Integer.MAX_VALUE;
     
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    StringTokenizer st;
 	    
 	    N = Integer.parseInt(br.readLine());
-	    arr = new int[N+1];
-	    visited = new boolean[N+1];
+	    
+	    arr = new int[N + 1];
+	    st = new StringTokenizer(br.readLine(), " ");
+	    for(int i = 1 ; i <= N ; i++) {
+	        arr[i] = Integer.parseInt(st.nextToken());
+	    }
 	    
 	    A = new ArrayList[N+1];
 	    for(int i = 1 ; i <= N ; i++) {
 	        A[i] = new ArrayList<>();
 	    }
 	    
-	    st = new StringTokenizer(br.readLine(), " ");
-	    for(int i = 1 ; i <= N ; i++) {
-	        arr[i] = Integer.parseInt(st.nextToken());
-	    }
-	    
 	    for(int i = 1 ; i <= N ; i++) {
 	        String[] str = br.readLine().split(" ");
+	        
+	        int cnt = Integer.parseInt(str[0]);
+	        
 	        for(int j = 1 ; j < str.length ; j++) {
 	            int num = Integer.parseInt(str[j]);
-	            
 	            A[i].add(num);
 	            A[num].add(i);
 	        }
 	    }
 	    
-	    dfs(0);
-	    
-	    if(min == Integer.MAX_VALUE) {
-	        min = -1;
-	    }
-	    
-	    System.out.println(min);
+	    selected = new boolean[N+1];
+	    dfs(1, 0);
+	    System.out.println(diff == Integer.MAX_VALUE ? -1 : diff);
 	}
 	
-	private static void dfs(int idx) {
+	private static void dfs(int idx, int depth) {
 	    if(idx == N) {	        
-	        List<Integer> group1 = new ArrayList<>();
-	        List<Integer> group2 = new ArrayList<>();
-	        
+	        ArrayList<Integer> list1 = new ArrayList<>();
+	        ArrayList<Integer> list2 = new ArrayList<>();
+
 	        for(int i = 1 ; i <= N ; i++) {
-	            if(visited[i])
-	                group1.add(i);
+	            if(selected[i])
+	                list1.add(i);
 	            else
-	                group2.add(i);
+	                list2.add(i);
 	        }
+            
+	        if(list1.size() == 0 || list2.size() == 0)  return;
+	        if(!isConnected(list1) || !isConnected(list2))  return;
 	        
-	        if(group1.size() == 0 || group2.size() == 0)
-	            return;
+	        int sum1 = 0;
+	        int sum2 = 0;
+	        for(int x : list1)  sum1 += arr[x];
+	        for(int x : list2)  sum2 += arr[x];
 	        
-	        if(bfs(group1) &&  bfs(group2))
-	            getDiff();
-	        
+    	    diff = Math.min(diff, Math.abs(sum1 - sum2));
 	        return;
 	    }
 	    
-	    visited[idx] = true;
-	    dfs(idx + 1);
-	    
-	    visited[idx] = false;
-	    dfs(idx + 1);
+	    dfs(idx + 1, depth);
+        
+	    selected[idx] = true;
+	    dfs(idx + 1, depth + 1);
+	    selected[idx] = false;
 	}
 	
-	private static boolean bfs(List<Integer> list) {
+	private static boolean isConnected(List<Integer> list) {
+	    boolean[] visited = new boolean[N+1];
+	    visited[list.get(0)] = true;
+        
 	    Queue<Integer> q = new ArrayDeque<>();
 	    q.add(list.get(0));
-	    
-	    boolean[] chk = new boolean[N+1];
-	    chk[list.get(0)] = true;
 	    
 	    while(!q.isEmpty()) {
 	        int now = q.poll();
 	        
 	        for(int next : A[now]) {
-	            if(list.contains(next) && !chk[next]) {
-	                chk[next] = true;
+	            if(!visited[next] && list.contains(next)) {
+	                visited[next] = true;
 	                q.add(next);
 	            }
 	        }
 	    }
 	    
-	    for(int i : list) {
-	        if(!chk[i]) {
+	    for(int num : list) {
+	        if(!visited[num])
 	            return false;
-	        }
 	    }
 	    
 	    return true;
-	}
-	
-	private static void getDiff() {
-	    int sum1 = 0;
-	    int sum2 = 0;
-	    
-	    for(int i = 1 ; i <= N ; i++) {
-	        if(visited[i])  sum1 += arr[i];
-	        else            sum2 += arr[i];
-	    }
-	    
-	    int diff = (int)Math.abs(sum1 - sum2);
-	    min = Math.min(min, diff);
 	}
 }
