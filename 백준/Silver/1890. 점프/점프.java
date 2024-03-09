45ceb9bb3a815e1eca5826fe68a33754be267c62
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-03-09 21:36:18 +0900

    [Silver I] Title: 점프, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -2,35 +2,41 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int N;
+    static int[][] board;
+    static long[][] dp;
+    
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int N = Integer.parseInt(br.readLine());
-		int[][] map = new int[N + 1][N + 1];
-        
-		
-		// 입력 받기
-		for(int i = 1 ; i <= N ; i++) {
-		    st = new StringTokenizer(br.readLine()," ");
-		    for(int j = 1 ; j <= N ; j++) {
-		        map[i][j] = Integer.parseInt(st.nextToken());
-		    }
-		}
-		
-		long[][] dp  = new long[N + 1][N + 1];
-		dp[1][1] = 1;
-		for(int i = 1 ; i <= N ; i++) {
-		    for(int j = 1 ; j <= N ; j++) {
-		        int next = map[i][j];
-		        
-		        if(i == N && j == N) continue;
-		        
-		        if(j + next <= N)    dp[i][j + next] += dp[i][j];
-		        if(i + next <= N)    dp[i + next][j] += dp[i][j];
-		    }
-		}		
-		
-		System.out.println(dp[N][N]);
+	    StringTokenizer st;
+	    
+	    N = Integer.parseInt(br.readLine());
+	    board = new int[N][N];
+	    
+	    for(int i = 0 ; i < N ; i++) {
+	        st = new StringTokenizer(br.readLine(), " ");
+	        for(int j = 0 ; j < N ; j++) {
+	            board[i][j] = Integer.parseInt(st.nextToken());
+	        }
+	    }
+	    
+	    dp = new long[N][N];
+	    dp[0][0] = 1;
+	    
+	    for(int i = 0 ; i < N ; i++) {
+	        for(int j = 0 ; j < N ; j++) {
+	            if(i == N-1 && j == N-1)    continue;
+	            
+	            int val = board[i][j];
+	            
+                if(j + val < N)
+                    dp[i][j + val] += dp[i][j];
+                
+                if(i + val < N)
+                    dp[i + val][j] += dp[i][j];
+	        }
+	    }
+	    
+	    System.out.println(dp[N-1][N-1]);
 	}
 }
