ea382990e6882d08ee2772e4025622406765664e
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-03-22 23:09:36 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 228 ms, Memory: 18568 KB -BaekjoonHub

---
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] dx = {0, -1, -1, -1, 0, 1, 1, 1};
+    static int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+    
+    static int N,M;
+    static int[][] A;
+    static boolean[][] visited;
+    static Queue<int[]> clouds = new ArrayDeque<>();
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    
+	    N = Integer.parseInt(st.nextToken());
+	    M = Integer.parseInt(st.nextToken());
+	    
+	    A = new int[N][N];
+	    for(int i = 0 ; i < N ; i++) {
+	        st = new StringTokenizer(br.readLine(), " ");
+	        for(int j = 0 ; j < N ; j++) {
+	            A[i][j] = Integer.parseInt(st.nextToken());
+	        }
+	    }
+	    
+	    clouds.add(new int[] {N-1, 0});
+	    clouds.add(new int[] {N-1, 1});
+	    clouds.add(new int[] {N-2, 0});
+	    clouds.add(new int[] {N-2, 1});
+
+	    while(M --> 0) {
+	        st = new StringTokenizer(br.readLine(), " ");
+	        int d = Integer.parseInt(st.nextToken());
+	        int s = Integer.parseInt(st.nextToken());
+	        
+	        visited = new boolean[N][N];
+	        step12(d-1, s);
+	        step34();
+	        step5();
+	    }
+	    
+	    System.out.println(printSum());
+	}
+	
+	private static void step12(int d, int s) {
+	    for(int[] c : clouds) {
+	        c[0] = (N + c[0] + dx[d] * (s % N)) % N;
+	        c[1] = (N + c[1] + dy[d] * (s % N)) % N;
+	        A[c[0]][c[1]]++;
+	        visited[c[0]][c[1]] = true;
+	    }
+	}
+	
+	private static void step34() {
+	    while(!clouds.isEmpty()) {
+	        int[] now = clouds.poll();
+	        addWater(now[0], now[1]);
+	    }
+	}
+
+	private static void addWater(int x, int y) {
+	    for(int i = 1 ; i <= 7 ; i += 2) {
+	        int nx = x + dx[i];
+	        int ny = y + dy[i];
+	        
+	        if(inRange(nx, ny) && A[nx][ny] > 0)
+	            A[x][y]++;
+	    }
+	}
+	
+	private static void step5() {
+	    for(int i = 0 ; i < N ; i++) {
+	        for(int j = 0 ; j < N ; j++) {
+	            if(A[i][j] >= 2 && !visited[i][j]) {
+	                clouds.add(new int[] {i, j});
+	                A[i][j] -= 2;
+	            }
+	        }
+	    }
+	}
+	
+	private static int printSum() {
+	    int sum = 0;
+	    
+	    for(int[] i : A) {
+	        for(int j : i)
+	            sum += j;
+	    }
+	    
+        return sum;
+	}
+	
+	
+	private static boolean inRange(int x, int y) {
+	    return 0 <= x && x < N && 0 <= y && y < N;
+	}
+}
