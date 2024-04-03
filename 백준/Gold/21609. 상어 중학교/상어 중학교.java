abe7c8255204e794a3f113f2bbcb71586164d565
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-04-03 13:14:46 +0900

    [Gold II] Title: 상어 중학교, Time: 176 ms, Memory: 17392 KB -BaekjoonHub

--- 백준/Gold/21609. 상어 중학교/상어 중학교.java
+++ 백준/Gold/21609. 상어 중학교/상어 중학교.java
@@ -11,18 +11,17 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		init();
 		solution();
-		printResult();
+		System.out.println(sum);
 	}
 
 	static void solution() {
 		while (true) {
 			Block standardBlock = findMaxBlockGroupe();
-			if (standardBlock == null) {
-				return;
-			}
+			if (standardBlock == null) return;
 
 			sum += standardBlock.sum * standardBlock.sum;
 
+			
 			removeBlock(standardBlock);
 			applyGravity();
 			rotateMap();
@@ -36,146 +35,110 @@ public class Main {
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (map[i][j] == BLACK) {
-					continue;
-				}
-				if (map[i][j] == RAINBOW) {
-					continue;
-				}
-				if (map[i][j] == EMPTY) {
-					continue;
-				}
-				if (visited[i][j]) {
-					continue;
-				}
-
+			    if (visited[i][j]) continue;
+				if (map[i][j] == BLACK || map[i][j] == RAINBOW || map[i][j] == EMPTY)   continue;
+				
 				initRainBowVisited();
 				Block cur = bfsBlock(i, j, map[i][j]);
 
-				if (cur == null) {
-					continue;
-				}
+				if (cur == null)    continue;
+				
 				if (maxBlock.compareBlock(cur)) {
 					maxBlock = cur;
 				}
 			}
 		}
 
-		if (maxBlock.color == EMPTY) {
+		if (maxBlock.color == EMPTY)
 			return null;
-		}
 		return maxBlock;
 	}
 
 	static void initRainBowVisited() {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (map[i][j] == RAINBOW) {
+				if (map[i][j] == RAINBOW)
 					visited[i][j] = false;
-				}
 			}
 		}
 	}
 
 	static Block bfsBlock(int x, int y, int color) {
-		Queue<Integer> qx = new LinkedList<>();
-		Queue<Integer> qy = new LinkedList<>();
-		qx.offer(x);
-		qy.offer(y);
+		Queue<int[]> q = new ArrayDeque<>();
+		q.offer(new int[]{x, y});
+		
 		visited[x][y] = true;
+		
 		int sum = 1;
 		int rainbowSum = 0;
 
-		while (!qx.isEmpty()) {
-			int cx = qx.poll();
-			int cy = qy.poll();
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
 
-			for (int i = 0; i < 4; i++) {
-				int nx = cx + dx[i];
-				int ny = cy + dy[i];
+			for (int d = 0; d < 4; d++) {
+				int nx = now[0] + dx[d];
+				int ny = now[1] + dy[d];
 
-				if (outOfRange(nx, ny)) {
-					continue;
-				}
-				if (map[nx][ny] == BLACK) {
-					continue;
-				}
-				if (map[nx][ny] == EMPTY) {
-					continue;
-				}
-				if (visited[nx][ny]) {
-					continue;
-				}
+				if (!inRange(nx, ny) || visited[nx][ny])    continue;
+				if (map[nx][ny] == BLACK || map[nx][ny] == EMPTY)   continue;
+				
 				if (map[nx][ny] != color) {
 					if (map[nx][ny] == RAINBOW) {
 						rainbowSum++;
 						sum++;
+						
 						visited[nx][ny] = true;
-						qx.offer(nx);
-						qy.offer(ny);
+						q.add(new int[] {nx, ny});
 					}
 					continue;
 				}
 
 				sum++;
 				visited[nx][ny] = true;
-				qx.offer(nx);
-				qy.offer(ny);
+				q.add(new int[]{nx, ny});
 			}
 		}
 
-		if (sum < 2) {
+		if (sum < 2)
 			return null;
-		} else {
-			return new Block(x, y, color, sum, rainbowSum);
-		}
+		return new Block(x, y, color, sum, rainbowSum);
 	}
 
-	static boolean outOfRange(int x, int y) {
-		return x < 0 || x >= N || y < 0 || y >= N;
+	static boolean inRange(int x, int y) {
+		return 0 <= x && x < N && 0 <= y && y < N;
 	}
 
 	static void removeBlock(Block block) {
-		Queue<Integer> qx = new LinkedList<>();
-		Queue<Integer> qy = new LinkedList<>();
 		visited = new boolean[N][N];
-		qx.offer(block.x);
-		qy.offer(block.y);
 		visited[block.x][block.y] = true;
+		
+		Queue<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] {block.x, block.y});
+		
 		map[block.x][block.y] = EMPTY;
 
-		while (!qx.isEmpty()) {
-			int cx = qx.poll();
-			int cy = qy.poll();
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
+			
 			for (int i = 0; i < 4; i++) {
-				int nx = cx + dx[i];
-				int ny = cy + dy[i];
-				if (outOfRange(nx, ny)) {
-					continue;
-				}
-				if (map[nx][ny] == BLACK) {
-					continue;
-				}
-				if (map[nx][ny] == EMPTY) {
-					continue;
-				}
-				if (visited[nx][ny]) {
-					continue;
-				}
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
+				
+				if (!inRange(nx, ny) || visited[nx][ny]) continue;
+				if (map[nx][ny] == BLACK || map[nx][ny] == EMPTY) continue;
+				
 				if (map[nx][ny] != block.color) {
 					if (map[nx][ny] == RAINBOW) {
-						map[nx][ny] = EMPTY;
 						visited[nx][ny] = true;
-						qx.offer(nx);
-						qy.offer(ny);
+						q.add(new int[] {nx, ny});
+						map[nx][ny] = EMPTY;
 					}
 					continue;
 				}
 
-				map[nx][ny] = EMPTY;
 				visited[nx][ny] = true;
-				qx.offer(nx);
-				qy.offer(ny);
+				q.add(new int[] {nx, ny});
+				map[nx][ny] = EMPTY;
 			}
 		}
 	}
@@ -183,12 +146,10 @@ public class Main {
 	static void applyGravity() {
 		for (int i = 0; i < N; i++) {
 			for (int j = N - 2; j >= 0; j--) {
-				if (map[j][i] == BLACK) {
+				if (map[j][i] == BLACK || map[j][i] == EMPTY) {
 					continue;
 				}
-				if (map[j][i] == EMPTY) {
-					continue;
-				}
+				
 				moveBlock(j, i);
 			}
 		}
@@ -196,23 +157,19 @@ public class Main {
 
 	static void moveBlock(int x, int y) {
 		int cx = x;
+		
 		while (true) {
 			cx++;
 			if (cx >= N) {
 				break;
 			}
-			if (map[cx][y] == BLACK) {
-				break;
-			}
-			if (map[cx][y] != EMPTY) {
+			if (map[cx][y] == BLACK || map[cx][y] != EMPTY) {
 				break;
 			}
 		}
 		cx--;
 
-		if (cx == x) {
-			return;
-		}
+		if (cx == x)    return;
 
 		map[cx][y] = map[x][y];
 		map[x][y] = EMPTY;
@@ -222,37 +179,30 @@ public class Main {
 		int[][] map_copy = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				map_copy[i][j] = map[j][N - i - 1];
+				map_copy[i][j] = map[j][N - 1 - i];
 			}
 		}
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				map[i][j] = map_copy[i][j];
-			}
-		}
-	}
-
-	static void printResult() {
-		System.out.println(sum);
+		map = map_copy;
 	}
 
 	static void init() throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		
 		sum = 0;
 		map = new int[N][N];
+		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == -1) {
-					map[i][j] = BLACK;
-				} else if (map[i][j] == 0) {
-					map[i][j] = RAINBOW;
-				}
+				
+				if (map[i][j] == -1)    map[i][j] = BLACK;
+				else if (map[i][j] == 0)    map[i][j] = RAINBOW;
 			}
 		}
 	}
@@ -271,13 +221,13 @@ public class Main {
 		public boolean compareBlock(Block other) {
 			if (this.sum != other.sum)
 				return this.sum < other.sum;
-            
+
 			if (this.rainbowSum != other.rainbowSum)
 				return this.rainbowSum < other.rainbowSum;
 
 			if (this.x != other.x)
 				return this.x < other.x;
-
+            
 			return this.y < other.y;
 		}
 	}
