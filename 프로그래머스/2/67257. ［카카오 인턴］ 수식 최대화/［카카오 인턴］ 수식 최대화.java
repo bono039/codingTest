d40310c5ea7de6cf7caac402435c4f603f98b99c
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-04-03 23:38:40 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 13.18 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
 class Solution {
-    static char[] oper = {'+', '-', '*'};
-    static char[] output = new char[3]; // 순열 결과 저장 배열
+    static char[] operands = {'+', '-', '*'};
     static boolean[] chk = new boolean[3];
-
-    static List<Long> numList = new LinkedList<>();         // 숫자 담을 리스트
-    static List<Character> operList = new LinkedList<>();   // 연산자 담을 리스트
     
-    static long answer = 0; // 우승 시 받을 수 잇는 가장 큰 상금 금액
-        
-    public long solution(String expression) {        
+    static List<Long> numList = new ArrayList<>();
+    static List<Character> operList = new ArrayList<>();
+    
+    static long answer = 0;
+    
+    public long solution(String expression) {
         String tmp = "";
         
         for(char c : expression.toCharArray()) {
@@ -23,76 +22,79 @@ class Solution {
                 operList.add(c);
             }
         }
-        numList.add(Long.parseLong(tmp));   // 마지막 문자 삽입
+        numList.add(Long.parseLong(tmp));
         
-        dfs(0);
+        makeCombinations(0, "");
         return answer;
     }
     
-    // 순열 만드는 메서드
-    private static void dfs(int depth) {
-        if(depth == 3) {
-            solve();
+    private static void makeCombinations(int cnt, String str) {
+        if(cnt == 3) {
+            solve(str);
             return;
         }
         
         for(int i = 0 ; i < 3 ; i++) {
             if(!chk[i]) {
                 chk[i] = true;
-                output[depth] = oper[i];
-                dfs(depth + 1);
+                makeCombinations(cnt + 1, str + operands[i]);
                 chk[i] = false;
             }
         }
     }
     
-    // 연산
-    private static void solve() {
-        List<Long>   num = new LinkedList<>();
-        List<Character> oper = new LinkedList<>();
-                
-        num.addAll(numList);
-        oper.addAll(operList);
+    private static void solve(String str) {
+        List<Long> copyNums = new LinkedList<>();
+        List<Character> copyOps = new LinkedList<>();
         
-        for(int i = 0 ; i < 3 ; i++) {
-            char nowOp = output[i];   // 현재 우선순위 연산자
+        copyNums.addAll(numList);
+        copyOps.addAll(operList);
+        
+        for(int i = 0 ; i < str.length() ; i++) {
+            char nowOp = str.charAt(i);
             
-            for(int j = 0 ; j < oper.size() ; j++) {
-                if(oper.get(j).equals(nowOp)) { // 현재 우선순위 연산자와 맞는 경우
-                    long n1 = num.get(j);
-                    long n2 = num.get(j + 1);
-                    long res = calc(n1, n2, nowOp);
+            for(int j = 0 ; j < copyOps.size() ; j++) {
+                if(copyOps.get(j) == nowOp) {
+                    long res = calc(copyNums.get(j), copyNums.get(j + 1), nowOp);
                     
-                    num.remove(j + 1);  // 숫자 삭제
-                    num.remove(j);
-                    oper.remove(j);     // 연산자 삭제
+                    copyNums.remove(j + 1);
+                    copyNums.remove(j);
+                    copyOps.remove(j);
                     
-                    num.add(j, res);    // 연산 결과 넣기
-                    
-                    j--;    // 삭제했으니 인덱스 하나 줄이기
+                    copyNums.add(j, res);
+                    j--;
                 }
             }
         }
         
-        answer = Math.max(answer, Math.abs(num.get(0)));
+        answer = Math.max(answer, Math.abs(copyNums.get(0)));
     }
     
-    // 수식 계산
     private static long calc(Long a, Long b, char c) {
         long result = 0;
         
         switch(c) {
             case '+':
-                result = a + b;
+                result = a+b;
                 break;
             case '-':
-                result = a - b;
+                result = a-b;
                 break;
             case '*':
-                result = a * b;
+                result = a*b;
                 break;
         }
         
-        return result;        
+        return result;
+    }
+}
+
+class Node {
+    char op;
+    int idx;
+    
+    public Node(char op, int idx) {
+        this.op = op;
+        this.idx = idx;
     }
 }
