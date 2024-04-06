7fb422d4843195c9be01eeb3bcb29a0dd54bb0f7
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-04-07 00:25:02 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 600 ms, Memory: 89924 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,75 +2,80 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {1,-1,0,0};
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
     
-    static int N, M, breakCnt;
-    static int[][] map, dist;
+    static int N, M;
+    static int[][] map;
     static boolean[][][] visited;
     
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    
+	    N = Integer.parseInt(st.nextToken());
+	    M = Integer.parseInt(st.nextToken());
         
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        
-        map = new int[N][M];
-        dist = new int[N][M];
-
-        for(int i = 0 ; i < N ; i++) {
-            String str = br.readLine();
-            for(int j = 0 ; j < M ; j++) {
-                map[i][j] = str.charAt(j) - '0';
-            }
-        }
- 
-        if(N - 1 == 0 && M - 1 == 0) {
-            System.out.println(1);
-            System.exit(0);
-        }
-        
-        visited = new boolean[2][N + 1][M + 1];
-        System.out.println(bfs(0, 0, 0));
-    }
-    
-    private static int bfs(int breakCnt, int a, int b) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {a, b, 0});
-        
-        visited[breakCnt][a][b] = true;
-        
-        while(!queue.isEmpty()) {
-            int[] now = queue.poll();
-            
-            for(int d = 0 ; d < 4 ; d++) {
-                int x = now[0] + dx[d];
-                int y = now[1] + dy[d];
-                
-                if(x < 0 || y < 0 || x >= N || y >= M)    continue;
-
-                if(map[x][y] == 1) {
-                    if(now[2] == 0 && !visited[1][x][y]) {
-                        visited[now[2]][x][y] = true;
-                        dist[x][y] = dist[now[0]][now[1]] + 1;
-                        queue.offer(new int[] {x, y, 1});
-                    }    
-                }
-                else {
-                    if(!visited[now[2]][x][y]) {
-                        visited[now[2]][x][y] = true;
-                        dist[x][y] = dist[now[0]][now[1]] + 1;
-                        queue.offer(new int[] {x, y, now[2]});
-                    }
-                }
-                
-                if(x == N - 1 && y == M - 1) {
-                    return dist[x][y] + 1;
-                }
-            }
-            
-        } 
-        return -1;
-    }
+        if(N == 1 && M == 1) {
+	        System.out.println(1);
+	        return;
+	    }
+	    
+	    map = new int[N][M];
+	    visited = new boolean[2][N][M];
+	    
+	    for(int i = 0 ; i < N ; i++) {
+	        String str = br.readLine();
+	        for(int j = 0 ; j < M ; j++) {
+	            map[i][j] = str.charAt(j) - '0';
+	        }
+	    }
+	    
+	    System.out.println(bfs());
+	}
+	
+	private static int bfs() {
+	    int breakCnt = 0;
+	    
+	    Queue<int[]> q = new ArrayDeque<>();
+	    q.add(new int[] {0,0,0});
+	    
+	    visited[breakCnt][0][0] = true;
+	    
+	    while(!q.isEmpty()) {
+	        int[] now = q.poll();
+	        
+	        int x = now[0];
+	        int y = now[1];
+	        int crashed = now[2];
+	        
+	        for(int d = 0 ; d < 4 ; d++) {
+	            int nx = x + dx[d];
+	            int ny = y + dy[d];
+	            
+	            if(nx < 0 || nx >= N || ny < 0 || ny >= M)  continue;
+	            
+	            if(map[nx][ny] == 1) {
+	                if(crashed == 0 && !visited[1][nx][ny]) {
+	                    visited[crashed][nx][ny] = true;
+	                    map[nx][ny] = map[x][y] + 1;
+	                    q.add(new int[] {nx, ny, 1});
+	                }
+	            }
+	            else {
+	                if(!visited[crashed][nx][ny]) {
+	                    visited[crashed][nx][ny] = true;
+	                    map[nx][ny] = map[x][y] + 1;
+	                    q.add(new int[] {nx, ny, crashed});
+	                }
+	            }
+	            
+	            if(nx == N -1 && ny == M-1) {
+	                return map[nx][ny] + 1;
+	            }
+	        }
+	    }
+	    
+	    return -1;
+	}
 }
