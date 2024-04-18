e3df7c96c0899dff7ffc64d71cc3da93376ab907
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-04-18 15:48:04 +0900

    [Gold IV] Title: 알파벳, Time: 884 ms, Memory: 15096 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    
+    static int R,C;
+    static int[][] board;
+    static boolean[] alphabet = new boolean[26];
+    
+    static int answer = 1;
+    static int max = 0;
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    
+	    R = Integer.parseInt(st.nextToken());
+	    C = Integer.parseInt(st.nextToken());
+	    
+	    board = new int[R][C];
+	    
+	    for(int i = 0 ; i < R ; i++) {
+	        char[] ch = br.readLine().toCharArray();
+	        for(int j = 0 ; j < C ; j++) {
+	            board[i][j] = ch[j] - 'A';
+	        }
+	    }
+	    
+	    alphabet[board[0][0]] = true;
+	    dfs(0,0);
+	    System.out.println(max);
+	}
+	
+	private static void dfs(int x, int y) {
+	    for(int d = 0 ; d < 4 ; d++) {
+	        int nx = x + dx[d];
+	        int ny = y + dy[d];
+	        
+	        if(nx >= 0 && nx < R && ny >= 0 && ny < C) {
+	            if(!alphabet[board[nx][ny]]) {
+	                alphabet[board[nx][ny]] = true;
+	                answer++;
+	                dfs(nx, ny);
+	                alphabet[board[nx][ny]] = false;
+	                answer--;
+	            }
+	        }
+	    }
+	    
+	    max = Math.max(answer, max);
+	}
+}
