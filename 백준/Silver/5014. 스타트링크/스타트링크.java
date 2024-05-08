687dd381fa0d3da5f9c395f95ba13828573c73c1
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-05-08 16:49:33 +0900

    [Silver I] Title: 스타트링크, Time: 188 ms, Memory: 33900 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -3,53 +3,51 @@ import java.io.*;
 
 public class Main {
     static int F,S,G,U,D;
-    static int[] A;
-    static int cnt = 0;
-    static boolean isEnd = false;
+    static int[] arr;
     
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        F = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        G = Integer.parseInt(st.nextToken());
-        U = Integer.parseInt(st.nextToken());
-        D = Integer.parseInt(st.nextToken());
-        
-        A = new int[F + 1];
-        bfs(S);
-        
-        System.out.println(isEnd ? cnt : "use the stairs");
-    }
-    
-    private static void bfs(int num) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(num);
-        A[num] = 1;
-        
-        while(!queue.isEmpty()) {
-            int now = queue.poll();
-            
-            if(now == G) {
-                isEnd = true;
-                cnt = A[now] - 1;
-                return;
-            }
-            
-            if(now + U <= F) {
-                if(A[now + U] == 0) {
-                    A[now + U] = A[now] + 1;
-                    queue.add(now + U);
-                }
-            }
-            
-            if(now - D > 0) {
-                if(A[now - D] == 0) {
-                    A[now - D] = A[now] + 1;
-                    queue.add(now - D);
-                }
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    
+	    F = Integer.parseInt(st.nextToken());
+	    S = Integer.parseInt(st.nextToken());
+	    G = Integer.parseInt(st.nextToken());
+	    U = Integer.parseInt(st.nextToken());
+	    D = Integer.parseInt(st.nextToken());
+	    
+	    arr = new int[F + 1];
+	    int answer = bfs();
+	    
+	    System.out.println(answer == -1 ? "use the stairs" : answer);
+	}
+	
+	private static int bfs() {
+	    Queue<Integer> q = new ArrayDeque<>();
+	    q.add(S);
+	    
+	    arr[S] = 1;
+	    
+	    while(!q.isEmpty()) {
+	        int now = q.poll();
+	        
+	        if(now == G) {
+	            return arr[now] - 1;
+	        }
+	        
+	        if(now + U <= F) {
+	            if(arr[now + U] == 0) {
+	                arr[now + U] = arr[now] + 1;
+	                q.add(now + U);
+	            }
+	        }
+	        if(now - D >= 1) {
+	            if(arr[now - D] == 0) {
+	                arr[now - D] = arr[now] + 1;
+	                q.add(now - D);
+	            }
+	        }
+	    }
+	    
+	    return -1;
+	}
 }
