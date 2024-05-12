4c660745d4aa60052751302e63cdc95c1c037c9e
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-05-12 18:05:52 +0900

    [level 2] Title: 문자열 압축, Time: 0.05 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,34 +1,35 @@
 import java.util.*;
 
-class Solution {
+class Solution {    
     public int solution(String s) {
         int answer = s.length();
         int len = 1;
         
-        for(int i = 1 ; i <= s.length()/2 ; i++) {  
-            StringBuilder result = new StringBuilder();
-            String base = s.substring(0, i);    
+        for(int i = 1 ; i <= s.length() / 2 ; i++) {  
+            StringBuilder sb = new StringBuilder();
+            String base = s.substring(0,i);
             
             for(int j = i ; j <= s.length() ; j += i) {
-                int endIdx = Math.min(j + i, s.length());   
+                int eIdx = Math.min(j+i, s.length());
+                String next = s.substring(j, eIdx);
                 
-                String nextStr = s.substring(j, endIdx);
-                
-                if(base.equals(nextStr)) {
+                if(base.equals(next)) {
                     len++;
                 }
                 else {
                     if(len >= 2) {
-                        result.append(len);
+                        sb.append(len);
                     }
-                    result.append(base);
-                    base = nextStr; 
-                    len = 1;        
+                    
+                    sb.append(base);
+                    base = next;
+                    len = 1;
                 }
             }
-            result.append(base);    
-            answer = Math.min(answer, result.length());
-        }
+            
+            sb.append(base);
+            answer = Math.min(answer, sb.length());
+        }       
         
         return answer;
     }
