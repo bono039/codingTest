1c38015dd60311361d719d6ad5c765164fc782b1
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-05-13 10:03:43 +0900

    [Gold IV] Title: 플로이드, Time: 440 ms, Memory: 43000 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -2,52 +2,51 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static final int INF = 987654321;
+
     static int N,M;
-    static int[][] dp;  // 최단 거리 배열
-    static int INF = 987654321;
-    
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		N = Integer.parseInt(br.readLine());    // 도시 개수
-		M = Integer.parseInt(br.readLine());    // 버스 개수
-		
-		// 최단 거리 배열 초기화
-		dp = new int[N + 1][N + 1];
-		for(int i = 1 ; i <= N ; i++) {
-		    for(int j = 1 ; j <= N ; j++) {
-		        dp[i][j] = (i == j) ? 0 : INF;
-		    }
-		}
-		
-		// 입력 받아 그래프 데이터 채우기
-		while(M --> 0) {
-		    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		    
-		    int s = Integer.parseInt(st.nextToken());
-		    int e = Integer.parseInt(st.nextToken());
-		    int v = Integer.parseInt(st.nextToken());
-		    
-		    dp[s][e] = Math.min(dp[s][e], v);
-		}
-		
-		// 플로이드-워셜 수행
-		for(int K = 1 ; K <= N ; K++) {
-		    for(int S = 1 ; S <= N ; S++) {
-		        for(int E = 1 ; E <= N ; E++) {
-		            dp[S][E] = Math.min(dp[S][E], dp[S][K] + dp[K][E]);
-		        }
-		    }   
-		}
-		
-		// 출력하기
-		for(int i = 1 ; i <= N ; i++) {
-		    for(int j = 1 ; j <= N ; j++) {
-		        sb.append(dp[i][j] == INF ? 0 : dp[i][j]).append(" ");
-		    }
-		    sb.append("\n");
-		}
-		System.out.println(sb);
-	}
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st;
+
+            N = Integer.parseInt(br.readLine());
+            M = Integer.parseInt(br.readLine());
+
+            dp = new int[N][N];
+
+            for(int i = 0 ; i < N ; i++) {
+                for(int j = 0 ; j < N ; j++) {
+                    if(i != j)
+                        dp[i][j] = INF;
+                }
+            }
+
+            while(M --> 0) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int a = Integer.parseInt(st.nextToken()) -1;
+                int b = Integer.parseInt(st.nextToken()) -1;
+                int c = Integer.parseInt(st.nextToken());
+
+                dp[a][b] = Math.min(dp[a][b], c);
+            }
+            
+            for(int k = 0 ; k < N ; k++) {
+                for(int i = 0 ; i < N ; i++) {
+                    for(int j = 0 ; j < N ; j++) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0 ; i < N ; i++) {
+                for(int j = 0 ; j < N ; j++) {
+                    sb.append(dp[i][j] == INF ? 0 : dp[i][j]).append(" ");
+                }
+                sb.append("\n");
+            }
+            System.out.println(sb.toString());
+        }
 }
