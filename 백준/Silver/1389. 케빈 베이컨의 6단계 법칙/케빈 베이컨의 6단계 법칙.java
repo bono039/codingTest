d6584befe517cc2e1ed50cce1f1dd15975b1cf23
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-05-14 12:41:51 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 144 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,40 +1,38 @@
 import java.util.*;
 import java.io.*;
-// 양방향 인접 행렬
-// 중복 가능.
+
 public class Main {
     static int N, M;
     static int INF = 10000001;
-    static int[][] distance;
+    static int[][] dp;
     
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		distance = new int[N + 1][N + 1];
+	    N = Integer.parseInt(st.nextToken());
+	    M = Integer.parseInt(st.nextToken());
+
+		dp = new int[N + 1][N + 1];
 		for(int i = 1 ; i <= N ; i++) {
 		    for(int j = 1 ; j <= N ; j++) {
-		        if(i == j) distance[i][j] = 0;
-		        else       distance[i][j] = INF;
+		        dp[i][j] = (i == j) ? 0 : INF;
 		    }
 		}
-		
-		for(int i = 1 ; i <= M ; i++) {
-		    st = new StringTokenizer(br.readLine()," ");
-		    int a = Integer.parseInt(st.nextToken());
-		    int b = Integer.parseInt(st.nextToken());
-		    
-		    distance[a][b] = distance[b][a] = 1;
+
+		while(M --> 0) {
+		     st = new StringTokenizer(br.readLine(), " ");
+		     int a = Integer.parseInt(st.nextToken());
+		     int b = Integer.parseInt(st.nextToken());
+		     
+		     dp[a][b] = dp[b][a] = 1;
 		}
 		
-		for(int k = 1 ; k <= N ; k++) {
+		for(int k = 1 ;  k <= N ; k++) {
 		    for(int i = 1 ; i <= N ; i++) {
 		        for(int j = 1 ; j <= N ; j++) {
-		            if(distance[i][j] > distance[i][k] + distance[k][j]) {
-		                distance[i][j] = distance[i][k] + distance[k][j];
+		            if(dp[i][j] > dp[i][k] + dp[k][j]) {
+		                dp[i][j] = dp[i][k] + dp[k][j];
 		            }
 		        }
 		    }
@@ -42,18 +40,18 @@ public class Main {
 		
 		int min = Integer.MAX_VALUE;
 		int answer = -1;
-
+		
 		for(int i = 1 ; i <= N ; i++) {
 		    int tmp = 0;
-		    
 		    for(int j = 1 ; j <= N ; j++) {
-		        tmp += distance[i][j];
+		        tmp += dp[i][j];
 		    }
+		    
 		    if(min > tmp) {
 		        min = tmp;
 		        answer = i;
 		    }
-	    }
-	    System.out.println(answer);
+		}
+		System.out.println(answer);
 	}
 }
