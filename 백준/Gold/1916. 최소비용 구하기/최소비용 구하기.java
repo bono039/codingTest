7b2ba957dcbdd5eec8b0640ce8983b93366d32f5
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-05-18 16:44:50 +0900

    [Gold V] Title: 최소비용 구하기, Time: 544 ms, Memory: 51384 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -3,75 +3,79 @@ import java.io.*;
 
 public class Main {
     static int N, M;
-    static int[] D;             // 최단 경로 배열
-    static ArrayList<Node>[] A; // 인접 리스트
-    static boolean visited[];
+    static ArrayList<Node>[] A;
+    static boolean[] visited;
+    
+    static int[] D;
+    static int MAX = Integer.MAX_VALUE;
     
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
 		
-		visited = new boolean[N+1];
-		D = new int[N+1];
-		Arrays.fill(D, Integer.MAX_VALUE);
-		
-		A = new ArrayList[N+1];
-		for(int i = 0 ; i <= N ; i++) {
+		A = new ArrayList[N + 1];
+		for(int i = 1 ; i <= N ; i++) {
 		    A[i] = new ArrayList<>();
 		}
+		visited = new boolean[N + 1];
+		
+		D = new int[N + 1];
+		Arrays.fill(D, Integer.MAX_VALUE);
 		
 		while(M --> 0) {
-		    st = new StringTokenizer(br.readLine()," ");
-		    int S = Integer.parseInt(st.nextToken());
-		    int E = Integer.parseInt(st.nextToken());
-		    int V = Integer.parseInt(st.nextToken());
+		    st = new StringTokenizer(br.readLine(), " ");
 		    
-		    A[S].add(new Node(E, V));
+		    int s = Integer.parseInt(st.nextToken());
+		    int e = Integer.parseInt(st.nextToken());
+		    int v = Integer.parseInt(st.nextToken());
+		    
+		    A[s].add(new Node(e, v));
 		}
 		
-		st = new StringTokenizer(br.readLine()," ");
-		int startCity = Integer.parseInt(st.nextToken());
-		int endCity = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine(), " ");
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
 		
-        System.out.println(dijkstra(startCity, endCity));
+		System.out.println(dijkstra(start, end));
 	}
 	
-	
-	public static int dijkstra(int start, int end) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.add(new Node(start, 0));
-		
-        D[start] = 0;
-		
-		while(!pq.isEmpty()) {
-		    Node nowNode = pq.poll();
-		    if(visited[nowNode.node]) continue; // 중복 방지
-		    visited[nowNode.node] = true;
-		    
-		    for(Node next : A[nowNode.node]) {
-		        if(D[next.node] > D[nowNode.node] + next.value) {
-		            D[next.node] = D[nowNode.node] + next.value;
-		            pq.offer(new Node(next.node, D[next.node]));
-		        }
-		    }
-		}
-		return D[end];
+	private static int dijkstra(int start, int end) {
+	    PriorityQueue<Node> pq = new PriorityQueue<>();
+	    pq.add(new Node(start, 0));
+	    
+	    D[start] = 0;
+	    
+	    while(!pq.isEmpty()) {
+	        Node now = pq.poll();
+	        
+	        if(visited[now.node])   continue;
+	        visited[now.node] = true;
+	        
+	        for(Node next : A[now.node]) {
+	            if(D[next.node] > D[now.node] + next.val) {
+	                D[next.node] = D[now.node] + next.val;
+	                pq.add(new Node(next.node, D[next.node]));
+	            }
+	        }
+	    }
+	    return D[end];
+	    
 	}
 }
 
-class Node implements Comparable<Node>{
-    int node, value;
+class Node implements Comparable<Node> {
+    int node, val;
     
-    Node(int node, int value) {
+    public Node(int node, int val) {
         this.node = node;
-        this.value = value;
+        this.val = val;
     }
     
     @Override
     public int compareTo(Node e) {
-        return this.value - e.value;
+        return this.val - e.val;
     }
 }
