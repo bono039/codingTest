0ac5f1021bf5ec33b025d0e8bf78fc2c7f8d1f57
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-06-01 21:04:12 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 188 ms, Memory: 15676 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -2,63 +2,55 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N, max = Integer.MIN_VALUE;
-	static int[] durability, weight;
+    static int N, answer;
+    static int[] S,W;
     
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		
-		durability = new int[N];
-		weight = new int[N];
-		
-		for(int i = 0 ; i < N ; i++) {
-		    st = new StringTokenizer(br.readLine(), " ");
-		    
-			durability[i] = Integer.parseInt(st.nextToken());
-			weight[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		back(0);
-		System.out.println(max);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    
+	    N = Integer.parseInt(br.readLine());
+	    
+	    S = new int[N];
+	    W = new int[N];
+	    
+	    for(int i = 0 ; i < N ; i++) {
+	        st = new StringTokenizer(br.readLine(), " ");
+	        S[i] = Integer.parseInt(st.nextToken());
+	        W[i] = Integer.parseInt(st.nextToken());
+	    }
+	    
+	    dfs(0, 0);
+	    System.out.println(answer);
 	}
 	
-	private static void back(int depth) {
-		if(depth == N) {
-			int cnt = 0;
-			
-			for(int i = 0 ; i < N ; i++) 
-				if(durability[i] <= 0)
-					cnt++;
-			
-			max = Math.max(max, cnt);
-			return;
-		}
-        
-		if(durability[depth] <= 0)
-			back(depth + 1);
-		else {
-			boolean broken = false;
+	private static void dfs(int depth, int cnt) {
+	    if(depth == N) {
+	        answer = Math.max(answer, cnt);
+	        return;
+	    }
+	    
+	    if(S[depth] <= 0 || cnt == N-1) {
+	        dfs(depth + 1, cnt);
+	        return;
+	    }
+	    
+	    int tmpCnt = cnt;
+	    
+        for(int i = 0 ; i < N ; i++) {
+            if(i == depth || S[i] <= 0) continue;
             
-			for(int i = 0 ; i < N ; i++) {
-				if(i == depth || durability[i] <= 0) 
-					continue;
-                
-				broken = true;
-				
-				durability[i] -= weight[depth];
-				durability[depth] -= weight[i];
-                
-				back(depth + 1);
-                
-				durability[i] += weight[depth];
-				durability[depth] += weight[i];
-			}
+            S[i] -= W[depth];
+            S[depth] -= W[i];
             
-			if(!broken)
-				back(depth + 1);
-		}
+            if(S[depth] <= 0)   cnt++;
+            if(S[i] <= 0)   cnt++;
+            
+            dfs(depth + 1, cnt);
+            
+            S[i] += W[depth];
+            S[depth] += W[i];
+            cnt = tmpCnt;
+        }
 	}
 }
