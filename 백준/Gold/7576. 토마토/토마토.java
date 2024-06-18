79a39f984308c8229e574e7b3f14b8187f89fae1
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-06-18 22:15:57 +0900

    [Gold V] Title: 토마토, Time: 580 ms, Memory: 103828 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,14 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
     
-    static int M, N;
+    static int N,M;
     static int[][] map;
     
     static Queue<int[]> q = new ArrayDeque<>();
-    static int max = Integer.MIN_VALUE;
+    static int answer = Integer.MIN_VALUE;
     
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,14 +19,13 @@ public class Main {
 	    N = Integer.parseInt(st.nextToken());
 	    
 	    map = new int[N][M];
-	    
 	    for(int i = 0 ; i < N ; i++) {
 	        st = new StringTokenizer(br.readLine(), " ");
 	        for(int j = 0 ; j < M ; j++) {
 	            map[i][j] = Integer.parseInt(st.nextToken());
 	            
 	            if(map[i][j] == 1) {
-	                q.add(new int[] {i, j});
+	                q.add(new int[] {i,j});
 	            }
 	        }
 	    }
@@ -39,32 +38,29 @@ public class Main {
 	                System.out.println(-1);
 	                return;
 	            }
-	            max = Math.max(max, map[i][j]);
+	            answer = Math.max(answer, map[i][j]);
 	        }
 	    }
 	    
-	    System.out.println(max == 1 ? 0 : max - 1);
+	    System.out.println(answer == 1 ? 0 : answer - 1);
 	}
 	
 	private static void bfs() {
 	    while(!q.isEmpty()) {
 	        int[] now = q.poll();
 	        
-	        for(int d = 0 ;d < 4 ; d++) {
+	        for(int d = 0 ; d < 4 ; d++) {
 	            int nx = now[0] + dx[d];
 	            int ny = now[1] + dy[d];
 	            
-	            if(!inRange(nx, ny))    continue;
+	            if(nx < 0 || nx >= N || ny < 0 || ny >= M)  continue;
 	            
 	            if(map[nx][ny] == 0) {
-    	            map[nx][ny] = map[now[0]][now[1]] + 1;
-    	            q.add(new int[] {nx, ny});	                
+	                map[nx][ny] = map[now[0]][now[1]] + 1;
+	                answer = map[nx][ny];
+	                q.add(new int[]{nx, ny});
 	            }
 	        }
-	    }	    
-	}
-	
-	private static boolean inRange(int x, int y) {
-	    return (0 <= x && x < N && 0 <= y && y < M);
+	    }
 	}
 }
