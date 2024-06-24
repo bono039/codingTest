25e6743c7e6645d0bb7edf1615a9c2aa27650929
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-06-24 14:26:20 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,68 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N;
+    static int N, max;
     static int[] A, dp;
     
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    
+	    N = Integer.parseInt(br.readLine());	    
+	    A = new int[N];
+	    dp = new int[N];
         
-        N = Integer.parseInt(br.readLine());
-        A = new int[N];
-        dp = new int[N];
+	    st = new StringTokenizer(br.readLine(), " ");
+	    for(int i = 0 ; i < N ; i++) {
+	        A[i] = Integer.parseInt(st.nextToken());
+	    }
         
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0 ; i < N ; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        int LIS = 0;    // 처음 저장된 원소는 없으므로 값은 0
-        
-        for(int i = 0 ; i < N ; i++) {
-            int idx = binarySearch(A[i], 0, LIS, LIS + 1);
-            
-            // 찾지 못한 경우, 가장 마지막 위치에 원소 삽입하고 LIS 길이 늘림
-            if(idx == -1) {
-                dp[LIS++] = A[i];
-            }
-            // 찾은 경우, 해당 위치에 현재 값 삽입해 갱신
-            else {
-                dp[idx] = A[i];
-            }
-        }
-
-        int ans = 0;
-        for(int i : dp) {
-            if(i != 0) {
-                ans++;
-            }
-        }
-        System.out.println(ans);
-    }
-    
-    // binary Search 활용한 LIS
-    private static int binarySearch(int num, int s, int e, int size) {
-        int pos = 0;    // 원소 위치 기억용 변수
-        
-        while(s <= e) {
-            int mid = (s + e) / 2;
-            
-            if(num <= dp[mid]) {
-                pos = mid;  
-                e = mid - 1;
-            }
-            else {
-                s = mid + 1;
-            }
-        }
-        
-        // dp 테이블에 삽입될 위치를 못 찾은 경우
-        if(s == size)  {
-            return -1;
-        }
-        // dp 테이블에 삽입될 위치를 찾은 경우
-        else {
-            return pos;
-        }
-    }
+	    max = 1;
+	    
+	    for(int i = 0 ; i < N ; i++) {
+	        dp[i] = 1;
+	        
+	        for(int j = 0 ; j < i ; j++) {
+	            if(A[j] < A[i]) {
+	                dp[i] = Math.max(dp[i], dp[j] + 1);
+	                max = Math.max(max, dp[i]);
+	            }
+	        }
+	    }
+	    
+	    System.out.println(max);
+	}
 }
