c6de8b64a479e0191383e32d153cb36de35167b9
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-06-25 22:25:49 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.25 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -5,61 +5,63 @@ class Solution {
         
         List<long[]> list = new ArrayList<>();
         
-        long minX = Long.MAX_VALUE;
         long maxX = Long.MIN_VALUE;
-        long minY = Long.MAX_VALUE;
         long maxY = Long.MIN_VALUE;
-                
-        for(int i = 0 ; i < line.length ; i++) {
-            long a = line[i][0];
-            long b = line[i][1];
-            long n1 = line[i][2];
+        long minX = Long.MAX_VALUE;
+        long minY = Long.MAX_VALUE;
         
+        for(int i = 0 ; i < line.length ; i++) {
+            long A = line[i][0];
+            long B = line[i][1];
+            long E = line[i][2];
+                        
             for(int j = i+1 ; j < line.length ; j++) {
-                long c = line[j][0];
-                long d = line[j][1];
-                long n2 = line[j][2];
+                long C = line[j][0];
+                long D = line[j][1];
+                long F = line[j][2];
                 
-                long xUp = b*n2 - n1*d;
-                long xDown = a*d - b*c;
+                long xUp   = B*F - E*D;
+                long xDown = A*D - B*C;
                 
-                long yUp = n1*c - a*n2;
-                long yDown = a*d - b*c;
+                long yUp   = E*C - A*F;
+                long yDown = A*D - B*C;
                 
                 if(xDown != 0) {
                     double x = xUp / (double)xDown;
                     double y = yUp / (double)yDown;
                     
                     if(x == Math.ceil(x) && y == Math.ceil(y)) {
-                        list.add(new long[] {(long)x, (long)y});
+                        list.add(new long[]{(long)x, (long)y});
                         
                         minX = Math.min(minX, (long)x);
                         maxX = Math.max(maxX, (long)x);
                         minY = Math.min(minY, (long)y);
                         maxY = Math.max(maxY, (long)y);
-                    }
-                }
+                    }                    
+                }                
             }
         }
         
         boolean[][] answerTmp = new boolean[(int)(maxY - minY + 1)][(int)(maxX - minX + 1)];
         
-        for(long[] ints : list) {
-            int x = (int) (ints[0] - minX);
-            int y = (int) (ints[1] - maxY);
+        for(long[] info : list) {
+            int x = (int)(info[0] - minX);
+            int y = (int)(info[1] - maxY);
             
             answerTmp[Math.abs(y)][Math.abs(x)] = true;
         }
-        
+               
         String[] answer = new String[answerTmp.length];
+        int idx = 0;
         
-        int i = 0;
         for(boolean[] bs : answerTmp) {
             StringBuilder sb = new StringBuilder();
+            
             for(boolean b : bs) {
                 sb.append(b ? "*" : ".");
             }
-            answer[i++] = sb.toString();
+            
+            answer[idx++] = sb.toString();
         }
         return answer;
     }
