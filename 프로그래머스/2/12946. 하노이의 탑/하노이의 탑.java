f6b49d79fab68520c547bd5fd2300b92f3877393
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-07-07 16:39:45 +0900

    [level 2] Title: 하노이의 탑, Time: 5.56 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -3,27 +3,24 @@ import java.util.*;
 class Solution {
     static List<int[]> list = new ArrayList<>();
     
-    public int[][] solution(int n) {        
-        Hanoi(n, 1, 2, 3);
+    public int[][] solution(int n) {
+        Hanoi(n, 1,2,3);
         
         int[][] answer = new int[list.size()][2];
-        for(int i = 0 ; i < list.size() ; i++) {
-            int[] cmd = list.get(i);
-            answer[i][0] = cmd[0];
-            answer[i][1] = cmd[1];
+        for(int i = 0 ; i < answer.length ; i++) {
+            answer[i] = list.get(i);
         }
-        
         return answer;
     }
     
     private static void Hanoi(int n, int from, int via, int to) {
-        if(n == 1) {
-            list.add(new int[] {from, to});
-            return;
-        }
-
-        Hanoi(n - 1, from, to, via);        // 1 -> 2
-        list.add(new int[] {from, to});     // 1 -> 3
-        Hanoi(n - 1, via, from, to);        // 2 -> 3
+         if(n == 1) {
+             list.add(new int[]{from, to});
+             return;
+         }
+        
+         Hanoi(n-1, from, to, via);     // 1->2
+         list.add(new int[]{from, to}); // 1->3
+         Hanoi(n-1, via, from, to);     // 2->3
     }
 }
