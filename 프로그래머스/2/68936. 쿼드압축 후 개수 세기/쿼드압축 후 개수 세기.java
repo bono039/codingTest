4b7aa7664f34a27a847573cb33dd5ae650218ecd
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-07-07 15:34:53 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 19.01 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -3,42 +3,61 @@ import java.util.*;
 class Solution {
     static int[][] arr;
     static int len;
-    static int zeroCnt = 0, oneCnt = 0;
     
     public int[] solution(int[][] arr) {
         this.arr = arr;
         len = arr.length;
         
-        solve(0, 0, len);
-        return new int[]{zeroCnt, oneCnt};
+        solve(0,0,len);    
+        
+        int[] answer = new int[2];
+        for(int i = 0 ; i < len ; i++) {
+            for(int j = 0 ; j < len ; j++) {
+                if(arr[i][j] == 0)      answer[0]++;
+                else if(arr[i][j] == 1) answer[1]++;
+            }
+        }
+        return answer;
     }
     
     private static void solve(int x, int y, int size) {
-        if(check(x, y, size)) {
-            if(arr[x][y] == 0)      zeroCnt++;
-            else if(arr[x][y] == 1) oneCnt++;
+        int zeroCnt = 0;
+        int oneCnt = 0;
+        
+        for(int i = x ; i < x+size ; i++) {
+            for(int j = y ; j < y+size ; j++) {
+                if(arr[i][j] == 0)      zeroCnt++;
+                else if(arr[i][j] == 1) oneCnt++;
+            }
+        }
+        
+        if(zeroCnt == 0) {
+            for(int i = x ; i < x+size ; i++) {
+                for(int j = y ; j < y+size ; j++) {
+                    if(i == x && j == y)    continue;
+                    
+                    arr[i][j] = -1;
+                }
+            }
             
             return;
         }
         
-        int newSize = size/2;
-        
-        solve(x,         y,         newSize);
-        solve(x+newSize, y,         newSize);
-        solve(x,         y+newSize, newSize);
-        solve(x+newSize, y+newSize, newSize);
-    }
-    
-    private static boolean check(int x, int y, int size) {
-        int v = arr[x][y];
-        
-        for(int i = x ; i < x+size ; i++) {
-            for(int j = y ; j < y+size ; j++) {
-                if(arr[i][j] != v)
-                    return false;
-            }
+        if(oneCnt == 0) {
+            for(int i = x ; i < x+size ; i++) {
+                for(int j = y ; j < y+size ; j++) {
+                    if(i == x && j == y)    continue;
+                    
+                    arr[i][j] = -1;
+                }
+            } 
+            
+            return;
         }
         
-        return true;
+        solve(x, y, size/2);
+        solve(x+size/2, y, size/2);
+        solve(x, y+size/2, size/2);
+        solve(x+size/2, y+size/2, size/2);
     }
 }
