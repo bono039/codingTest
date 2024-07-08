8389f88f0a354cc5e0dc966f7dce502a5c1cdd96
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-07-08 16:37:27 +0900

    [level 1] Title: 모의고사, Time: 1.23 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,36 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answers) {
-        
-        int[] p1 = {1,2,3,4,5};
-        int[] p2 = {2,1,2,3,2,4,2,5};
-        int[] p3 = {3,3,1,1,2,2,4,4,5,5};
-        
+    static int[] n1 = {1,2,3,4,5};
+    static int[] n2 = {2,1,2,3,2,4,2,5};
+    static int[] n3 = {3,3,1,1,2,2,4,4,5,5};
+    
+    public int[] solution(int[] answers) {    
         int[] scores = new int[3];
-        
+            
         for(int i = 0 ; i < answers.length ; i++) {
-            if(answers[i] == p1[i % p1.length]) scores[0]++;
-            if(answers[i] == p2[i % p2.length]) scores[1]++;
-            if(answers[i] == p3[i % p3.length]) scores[2]++;
+            if(answers[i] == n1[i%5])  scores[0]++;
+            if(answers[i] == n2[i%8])  scores[1]++;
+            if(answers[i] == n3[i%10]) scores[2]++;
         }
-        
-        // 최대 점수 구하기
+               
         int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
         
-        // 최대 점수 가진 수포자 리스트 생성
-        List<Integer> answer = new ArrayList<>();
-        for(int i = 0 ; i < scores.length ; i++) {
-            if(max == scores[i])
-                answer.add(i + 1);
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i < 3 ; i++) {
+            if(scores[i] == max)
+                list.add(i+1);
         }
         
-        // 최대 점수 가진 수포자 리스트 -> 배열로 변경해 출력하기
-        int[] ans = new int[answer.size()];
-        for(int i = 0 ; i < answer.size() ; i++) {
-            ans[i] = answer.get(i);
+        int[] answer = new int[list.size()];
+        for(int i = 0 ; i < answer.length ; i++) {
+            answer[i] = list.get(i);
         }
-        
-        return ans;
+        return answer;
     }
 }
