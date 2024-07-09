41f495b76c1148d2e7eca0eb957bd8090d71d6fe
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-07-09 22:25:10 +0900

    [level 2] Title: 카펫, Time: 0.10 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -4,13 +4,14 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        // 소인수분해
         for(int i = yellow ; i >= 1 ; i--) {
             if(yellow % i == 0) {                
                 int sum = (i + yellow / i) *2 + 4;
-                if(sum <= brown) {
-                    answer[0] = Math.max(i + 2, yellow/i + 2);
-                    answer[1] = Math.min(i + 2, yellow/i + 2);
+                
+                if(sum == brown) {
+                    answer[0] = Math.max(i+2, yellow/i +2);
+                    answer[1] = Math.min(i+2, yellow/i +2);
+                    break;
                 }
             }
         }
