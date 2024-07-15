c7b78f0e2131b8a15ec4d82adc6f9258b7ac63f5
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-07-15 17:41:18 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 20.98 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,34 +1,33 @@
 import java.util.*;
 
 class Solution {
-    static char[] operands = {'+', '-', '*'};
+    static char[] op = {'+', '-', '*'};
     static boolean[] chk = new boolean[3];
     
     static List<Long> numList = new ArrayList<>();
-    static List<Character> operList = new ArrayList<>();
+    static List<Character> opList = new ArrayList<>();
     
     static long answer = 0;
     
     public long solution(String expression) {
         String tmp = "";
-        
         for(char c : expression.toCharArray()) {
             if(Character.isDigit(c)) {
                 tmp += c;
             }
             else {
                 numList.add(Long.parseLong(tmp));
+                opList.add(c);
                 tmp = "";
-                operList.add(c);
             }
         }
         numList.add(Long.parseLong(tmp));
         
-        makeCombinations(0, "");
+        dfs(0, "");
         return answer;
     }
     
-    private static void makeCombinations(int cnt, String str) {
+    private static void dfs(int cnt, String str) {
         if(cnt == 3) {
             solve(str);
             return;
@@ -37,7 +36,7 @@ class Solution {
         for(int i = 0 ; i < 3 ; i++) {
             if(!chk[i]) {
                 chk[i] = true;
-                makeCombinations(cnt + 1, str + operands[i]);
+                dfs(cnt+1, str+op[i]);
                 chk[i] = false;
             }
         }
@@ -48,53 +47,35 @@ class Solution {
         List<Character> copyOps = new LinkedList<>();
         
         copyNums.addAll(numList);
-        copyOps.addAll(operList);
+        copyOps.addAll(opList);
         
         for(int i = 0 ; i < str.length() ; i++) {
             char nowOp = str.charAt(i);
             
             for(int j = 0 ; j < copyOps.size() ; j++) {
                 if(copyOps.get(j) == nowOp) {
-                    long res = calc(copyNums.get(j), copyNums.get(j + 1), nowOp);
+                    long num = calc(copyNums.get(j), copyNums.get(j+1), nowOp);
                     
-                    copyNums.remove(j + 1);
+                    copyNums.remove(j+1);
                     copyNums.remove(j);
                     copyOps.remove(j);
                     
-                    copyNums.add(j, res);
+                    copyNums.add(j, num);
                     j--;
                 }
-            }
-        }
-        
+            }            
+        }        
         answer = Math.max(answer, Math.abs(copyNums.get(0)));
     }
     
-    private static long calc(Long a, Long b, char c) {
-        long result = 0;
+    private static long calc(long x, long y, char c) {
+        if(c == '+')
+            return x+y;
+        else if(c == '-')
+            return x-y;
+        else if(c == '*')
+            return x*y;
         
-        switch(c) {
-            case '+':
-                result = a+b;
-                break;
-            case '-':
-                result = a-b;
-                break;
-            case '*':
-                result = a*b;
-                break;
-        }
-        
-        return result;
-    }
-}
-
-class Node {
-    char op;
-    int idx;
-    
-    public Node(char op, int idx) {
-        this.op = op;
-        this.idx = idx;
+        return 0;
     }
 }
