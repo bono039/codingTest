38047d9105d4b69fe9c7435264102c8e81022dbd
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-07-18 21:54:48 +0900

    [level 1] Title: 신고 결과 받기, Time: 165.08 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,31 +4,26 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         
-        Map<String, Integer> reportCnt = new HashMap<>();
-        Map<String, Set<String>> infoMap = new HashMap<>();
+        Map<String, Integer> idxMap = new HashMap<>();
+        Map<String, HashSet<String>> map = new HashMap<>();
                 
-        for(String r : report) {
-            String[] s = r.split(" ");
-            
-            infoMap.putIfAbsent(s[0], new HashSet<>());
-            if(infoMap.get(s[0]).add(s[1]))
-                reportCnt.put(s[1], reportCnt.getOrDefault(s[1], 0) + 1);
-        }
-        
-        Set<String> noUsers = new HashSet<>();        
-        for(String s : reportCnt.keySet()) {
-            if(reportCnt.get(s) >= k) {
-                noUsers.add(s);
-            }
-        }
-        
         for(int i = 0 ; i < id_list.length ; i++) {
-            String user = id_list[i];
+            String name = id_list[i];
+            map.put(name, new HashSet<>());
+            idxMap.put(name, i);
+        }
+        
+        for(String r : report) {
+            String[] s = r.split(" ");            
+            map.get(s[1]).add(s[0]);
+        }
+
+        for(int i = 0 ; i < id_list.length ; i++) {
+            HashSet<String> send = map.get(id_list[i]);
             
-            if(infoMap.containsKey(user)) {
-                for(String x : infoMap.get(user)) {
-                    if(noUsers.contains(x))
-                        answer[i]++;
+            if(send.size() >= k) {
+                for(String x : send) {
+                    answer[idxMap.get(x)]++;
                 }
             }
         }
