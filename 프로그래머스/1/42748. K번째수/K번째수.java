60b5da5064ea4676fea7ad1111c8fc80e5159d43
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-07-20 23:11:07 +0900

    [level 1] Title: K번째수, Time: 0.25 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,20 +1,18 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {        
+    public int[] solution(int[] array, int[][] commands) {
         List<Integer> list = new ArrayList<>();
         
-        int outIdx = 0;
-        for(int i = 0 ; i < commands.length ; i++) {
-            int[] tmpArr = new int[commands[i][1] - commands[i][0] + 1];
-            int idx = 0;
-            for(int j = commands[i][0] - 1 ; j < commands[i][1] ; j++) {
-                tmpArr[idx++] = array[j];
+        for(int[] c : commands) {
+            List<Integer> tmp = new ArrayList<>();
+            for(int i = c[0] - 1 ; i <= c[1] - 1 ; i++) {
+                tmp.add(array[i]);
             }
-            Arrays.sort(tmpArr);
-            list.add(tmpArr[commands[i][2] - 1]);
+            Collections.sort(tmp);
+            list.add(tmp.get(c[2] - 1));
         }
-    
+            
         int[] answer = new int[list.size()];
         for(int i = 0 ; i < answer.length ; i++) {
             answer[i] = list.get(i);
