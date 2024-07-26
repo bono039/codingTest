56da76da0d0689b9424d8dce62d13f8f27692563
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-07-26 16:29:23 +0900

    [level 2] Title: 피로도, Time: 3.29 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,31 +1,30 @@
 import java.util.*;
 
 class Solution {
-    static int k;
+    static int k, len;
     static int[][] dungeons;
     static boolean[] chk;
-    
-    static int answer = 0;
+    static int max = 0;
     
     public int solution(int k, int[][] dungeons) {
         this.k = k;
         this.dungeons = dungeons;
+        len = dungeons.length;
         
-        chk = new boolean[dungeons.length];
+        chk = new boolean[len];
         dfs(0, k);
-                
-        return answer;
+        return max;
     }
     
-    private static void dfs(int cnt, int res) {        
-        for(int i = 0 ; i < dungeons.length ; i++) {
-            if(!chk[i] && dungeons[i][0] <= res) {
+    private static void dfs(int depth, int v) {        
+        for(int i = 0 ; i < len ; i++) {
+            if(!chk[i] && dungeons[i][0] <= v) {
                 chk[i] = true;
-                dfs(cnt + 1, res - dungeons[i][1]);
+                dfs(depth + 1, v - dungeons[i][1]);
                 chk[i] = false;
             }
         }
         
-        answer = Math.max(answer, cnt);
-    }
+        max = Math.max(max, depth);
+    }    
 }
