2b7369dd958745e75492bf1a63dd81c73addc42b
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-08-10 16:19:42 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 21.05 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {    
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        
+        Map<String, Person> map = new HashMap<>();
+        for(String x : enroll) {
+            map.put(x, new Person(x, null, 0));
+        }
+        
+        for(int i = 0 ; i < enroll.length ; i++) {
+            if(referral[i].equals("-")) { 
+                continue;
+            }
+            
+            map.get(enroll[i]).prev = map.get(referral[i]);
+        }
+        
+        for(int i = 0 ; i < seller.length ; i++) {
+            map.get(seller[i]).calc(amount[i] * 100);
+        }
+        
+        int[] answer = new int[enroll.length];
+        for(int i = 0 ; i < answer.length ; i++) {
+            answer[i] = map.get(enroll[i]).val;
+        }        
+        return answer;
+    }
+}
+
+class Person {
+    String now;
+    Person prev;
+    int val;
+    
+    public Person(String now, Person prev, int val) {
+        this.now = now;
+        this.prev = prev;
+        this.val = val;
+    }
+    
+    public void calc(int val) {
+        int profitToParent = val / 10;
+        this.val += (val - profitToParent);
+        
+        if(this.prev != null && profitToParent >= 1) {
+            this.prev.calc(profitToParent);
+        }
+    }
+}
