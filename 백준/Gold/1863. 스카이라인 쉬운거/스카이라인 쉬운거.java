50af85b41211c8a6af1d6432f245fd6c1205a450
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-08-13 16:27:49 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 220 ms, Memory: 18804 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    
+	    int N = Integer.parseInt(br.readLine());
+	    int ans = 0;
+	    Stack<Integer> stack = new Stack<>();
+	    
+	    for(int i = 0 ; i < N ; i++) {
+	        st = new StringTokenizer(br.readLine(), " ");
+	        int x = Integer.parseInt(st.nextToken());
+	        int y = Integer.parseInt(st.nextToken());
+	        
+	        while(!stack.isEmpty() && stack.peek() > y) {
+	            ans++;
+	            stack.pop();
+	        }
+	        
+	        if(!stack.isEmpty() && stack.peek() == y) {
+	            continue;
+	        }
+	        
+	        stack.push(y);
+	    }
+	    
+	    while(!stack.isEmpty()) {
+	        if(stack.peek() > 0) {
+	            ans++;
+	        }
+	        stack.pop();
+	    }
+	    
+	    System.out.println(ans);
+	}
+}
