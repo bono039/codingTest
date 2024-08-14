1d9c94c77fde6484c53cd8a43ae0189725ae57b3
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-08-15 00:11:12 +0900

    [Silver III] Title: 가희와 키워드, Time: 1304 ms, Memory: 220728 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -2,27 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        HashMap<String, Integer> keyword = new HashMap<>();
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        for(int i=0; i<n; i++) keyword.put(br.readLine(), 1);
-
-        for(int i=0; i<m; i++){
-            String [] str = br.readLine().split(",");
-
-            for(var post : str){
-                if(keyword.containsKey(post) && keyword.get(post) == 1){
-                    n--;
-                    keyword.put(post, 0);
-                }
-            }
-
-            System.out.println(n);
-        }
-    }
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    StringBuilder sb = new StringBuilder();
+	    
+	    int N = Integer.parseInt(st.nextToken());
+	    int M = Integer.parseInt(st.nextToken());
+	    
+	    HashMap<String, Integer> map = new HashMap<>();
+	    for(int i = 0 ; i < N ; i++) {
+	        String s = br.readLine();
+	        map.put(s, 1);
+	    }
+	    
+	    for(int i = 0 ; i < M ; i++) {
+	        String[] str = br.readLine().split(",");
+	        for(String s : str) {
+	            if(map.containsKey(s) && map.get(s) == 1) {
+	                N--;
+	                map.put(s, 0);
+	            }
+	        }
+	        
+	        sb.append(N).append("\n");
+	    }
+	    System.out.println(sb.toString());
+	}
 }
