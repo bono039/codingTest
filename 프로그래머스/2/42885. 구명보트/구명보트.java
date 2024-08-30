c763b5a09b10d6b503075e465add741b889b9ff0
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-08-30 14:34:47 +0900

    [level 2] Title: 구명보트, Time: 11.29 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,9 +1,10 @@
 import java.util.*;
 
-class Solution {    
+class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
+        int answer = 0;        
         Arrays.sort(people);
+        
         int idx = 0;
         
         for(int i = people.length - 1 ; i >= idx ; i--) {
