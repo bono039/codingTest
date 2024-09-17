dff2e8b154881e3e83a355d906f3b88b8920330e
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2024-09-18 00:08:41 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 436 ms, Memory: 61500 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,80 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	static int k,w,h;
-	static int[][] map;
-	static int[] dx= {1,-1,0,0}, dy= {0,0,1,-1};
-	static int[] hx = {1,2,2,1,-1,-2,-2,-1};
-	static int[] hy = {2,1,-1,-2,-2,-1,1,2};
-	public static void main(String[] args) throws IOException{
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    
+    // 말인 경우 대각선 4방향 이동
+    static int[] hx = {1,2,2,1,-1,-2,-2,-1};
+    static int[] hy = {2,1,-1,-2,-2,-1,1,2};
+    
+    static int K,W,H;
+    static int[][] map;
+    static boolean[][][] chk;
+    
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		k = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		w = Integer.parseInt(st.nextToken());
-		h = Integer.parseInt(st.nextToken());
-	
-		map = new int[h][w];
+		K = Integer.parseInt(br.readLine());
 		
-		for(int i=0; i<h; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<w; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		W = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		
+		map = new int[H][W];
+		for(int i = 0 ; i < H ; i++) {
+		    st = new StringTokenizer(br.readLine(), " ");
+		    for(int j = 0 ; j < W ; j++) {
+		        map[i][j] = Integer.parseInt(st.nextToken());
+		    }
 		}
 		
-		System.out.println(bfs(0,0));
+		chk = new boolean[H][W][K+1];
+		System.out.println(bfs());
 	}
 	
-	static int bfs(int x, int y) {
-		Queue<int[]> q = new LinkedList<>();
-		boolean[][][] check = new boolean[h][w][k+1];
-		
-		check[y][x][0] = true;
-		q.add(new int[] {x,y,0,0});
-		while(!q.isEmpty()) {
-			int[] pos = q.poll();
-			int px = pos[0], py = pos[1];
-			int chance = pos[2], move = pos[3];
-            
-			if(px==w-1 && py==h-1) {
-				 return move;
-			}
-            
-			for(int i=0; i<4; i++) {
-				int nx = px + dx[i];
-				int ny = py + dy[i];
-				
-				if(nx<0 || nx>w-1 || ny<0 || ny>h-1) continue;
-				if(check[ny][nx][chance])  continue;
-				
-				if(map[ny][nx] != 1) {
-					check[ny][nx][chance] = true;
-					q.add(new int[] {nx,ny,chance, move+1});
-				}
-			}
-
-			if(chance<k){
-				for(int i=0; i<8; i++) {
-					int nx = px + hx[i];
-					int ny = py + hy[i];
-					
-					if(nx<0 || nx>w-1 || ny<0 || ny>h-1) continue;
-					if(check[ny][nx][chance+1])  continue;
-					
-					if(map[ny][nx] != 1) {
-						check[ny][nx][chance+1] = true;
-						q.add(new int[] {nx,ny,chance+1, move+1});
-					}
-				}
-			}
-		}
-		return -1;
+	private static int bfs() {
+	    Queue<int[]> q = new ArrayDeque<>();
+	    q.add(new int[]{0,0,0,0});
+	    
+	    chk[0][0][0] = true;
+	    
+	    while(!q.isEmpty()) {
+	        int[] now = q.poll();
+	        int px = now[0];
+	        int py = now[1];
+	        int chance = now[2];
+	        int cnt = now[3];
+	        
+	        if(px == W-1 && py == H-1) {
+	            return cnt;
+	        }
+	        
+	        for(int d = 0 ; d < 4 ; d++) {
+	            int nx = px + dx[d];
+	            int ny = py + dy[d];
+	            
+	            if(nx < 0 || nx > W-1 || ny < 0 || ny > H-1)    continue;
+	            if(chk[ny][nx][chance]) continue;
+	            
+	            if(map[ny][nx] != 1) {
+	                chk[ny][nx][chance] = true;
+	                q.add(new int[] {nx, ny, chance, cnt + 1});
+	            }
+	        }
+	        
+	        if(chance < K) {
+	            for(int d = 0 ; d < 8 ; d++) {
+	                int nx = px + hx[d];
+	                int ny = py + hy[d];
+	                
+	                if(nx < 0 || nx > W-1 || ny < 0 || ny > H-1)    continue;
+	                if(chk[ny][nx][chance + 1]) continue;
+	                
+	                if(map[ny][nx] != 1) {
+	                    chk[ny][nx][chance + 1] = true;
+	                    q.add(new int[] {nx, ny, chance + 1, cnt + 1});
+	                }
+	            }
+	        }
+	    }
+	    
+	    return -1;
 	}
 }
