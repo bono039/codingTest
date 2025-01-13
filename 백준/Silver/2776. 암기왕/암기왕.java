bc61588e42a3350c29c8585c96ddb3d5dd78298f
Ui Jeong Han <67899934+bono039@users.noreply.github.com>
2025-01-14 00:12:02 +0900

    [Silver IV] Title: 암기왕, Time: 1368 ms, Memory: 288708 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -4,32 +4,38 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int T = Integer.parseInt(br.readLine());
-		
-		while(T --> 0) {
-		    Set<Integer> set = new HashSet<>();
-		    StringBuilder sb = new StringBuilder();
-		    
-		    int n1 = Integer.parseInt(br.readLine());
-		    StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		    while(n1 --> 0) {
-    		    set.add(Integer.parseInt(st.nextToken()));
-    		}
-    		
-    		int n2 = Integer.parseInt(br.readLine());
-    		st = new StringTokenizer(br.readLine()," ");
-		    while(n2 --> 0) {
-		        int num = Integer.parseInt(st.nextToken());
-		        
-    		    if(set.contains(num)) {
-		            sb.append(1).append("\n");
-    		    } else {
-    		        sb.append(0).append("\n");
-    		    }
-    		}    		
-    		
-    		System.out.print(sb.toString());
-		}	
+	    StringTokenizer st;
+	    StringBuilder sb = new StringBuilder();
+	    
+	    int T = Integer.parseInt(br.readLine());
+	    
+	    for(int x = 0 ; x < T ; x++) {
+    	    int N = Integer.parseInt(br.readLine());
+    	    Set<Integer> set = new HashSet<>();
+    	    
+    	    st = new StringTokenizer(br.readLine(), " ");
+    	    for(int i = 0 ; i < N ; i++) {
+    	        set.add(Integer.parseInt(st.nextToken()));
+    	    }
+    	    
+    	    int M = Integer.parseInt(br.readLine());
+    	    int[] arr = new int[M];
+    	    
+    	    st = new StringTokenizer(br.readLine(), " ");
+    	    for(int i = 0 ; i < M ; i++) {
+    	        arr[i] = Integer.parseInt(st.nextToken());
+    	    }
+    	    
+    	    for(int i = 0 ; i < M ; i++) {
+    	        if(set.contains(arr[i])) {
+    	            sb.append("1" + "\n");
+    	        }
+    	        else {
+    	            sb.append("0" + "\n");
+    	        }
+    	    }
+	    }
+	    
+	    System.out.println(sb.toString());
 	}
 }
